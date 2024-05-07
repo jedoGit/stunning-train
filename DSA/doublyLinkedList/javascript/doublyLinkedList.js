c39c6a2ef8fa035b3a081ba4c7a5e93cac7df614
jedoGit <ajvil@xps17.com>
2024-05-07 06:56:17 -0400

    DLL: Add push method

--- DSA/doublyLinkedList/javascript/doublyLinkedList.js
+++ DSA/doublyLinkedList/javascript/doublyLinkedList.js
@@ -13,4 +13,32 @@ class DoublyLinkedList {
     this.tail = this.head;
     this.length = 1;
   }
+
+  // Push method always insert new node
+  // on the end of the LL and return
+  // the updated LinkedList
+  push(value) {
+    const newNode = new Node(value);
+
+    // Edge case, if head and tail points to null, meaning
+    // LinkedList is empty
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      // LinkedList has at least 1 node
+      // First, point the tail.next to the newNode
+      this.tail.next = newNode;
+      // Then, point the newNode.prev to tail
+      newNode.prev = this.tail;
+      // Lastly, point the tail to the newNode
+      this.tail = newNode;
+    }
+
+    // Increment the LL length
+    this.length++;
+
+    // Return this instance of LL
+    return this;
+  }
 }

d603f021eede8301215fe9dc3b95eba9dda03e6a
jedoGit <ajvil@xps17.com>
2024-10-11 20:23:56 -0400

    Update comments

--- DSA/Sort/javascript/insertionSort.js
+++ DSA/Sort/javascript/insertionSort.js
@@ -5,15 +5,17 @@ function insertionSort(array) {
   let temp = 0;
   // We'll loop through the array. We always start at the second element of the array
   for (let i = 1; i < array.length; i++) {
-    // Save the 2nd element of the array to a temp variable
+    // Save the 2nd element of the array to a temp variable. We'll use this
+    // to compare to the elements
     temp = array[i];
-    // Now, we compare the element were currently pointing to (temp) with
+    // Now, we compare the element we're currently pointing to (temp) with
     // the elements to the left of it.
     // If the value to the left of temp is greater than than temp, update the value
     // to the right with the value of the left. Keep going and decrement j until j is 0.
     for (var j = i - 1; array[j] > temp && j > -1; j--) {
       array[j + 1] = array[j];
     }
+    // When we exited the for loop, the value of j has been decremented to -1. When that happened, it exited the for-loop, j+1 here is index 0.
     // At this point, we've compared temp to all the elements to the left of it. After the we
     // exit the for loop above, we've decrement j and this j+1 index is the open spot that we
     // insert temp to.

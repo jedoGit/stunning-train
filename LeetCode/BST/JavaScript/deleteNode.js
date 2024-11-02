b42b82c4ce7e087332b3b1c7046406c08e7c411e
jedoGit <ajvillardo@gmail.com>
2024-11-02 16:20:25 -0400

    Delete Node

--- LeetCode/BST/JavaScript/deleteNode.js
+++ LeetCode/BST/JavaScript/deleteNode.js
@@ -98,3 +98,45 @@ var deleteNode = function (root, key) {
 
   return root;
 };
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} key
+ * @return {TreeNode}
+ */
+var deleteNode = function (root, key) {
+  if (!root) return root;
+
+  function removeNode(root, key) {
+    if (!root) return root;
+
+    if (key < root.val) {
+      root.left = removeNode(root.left, key);
+    } else if (key > root.val) {
+      root.right = removeNode(root.right, key);
+    } else {
+      if (!root.left && !root.right) return null;
+      if (!root.left) return root.right;
+      if (!root.right) return root.left;
+
+      root.val = min(root.right);
+      root.right = removeNode(root.right, root.val);
+    }
+    return root;
+  }
+
+  return removeNode(root, key);
+};
+
+function min(root) {
+  if (!root.left) return root.val;
+  return min(root.left);
+}

ce2887df9ceb0fee476a9938c4c456e674f4de2d
jedoGit <ajvillardo@gmail.com>
2024-12-10 20:52:54 -0500

    Product Except Self

--- LeetCode/ArrayStrings/JavaScript/productExceptSelf.js
+++ LeetCode/ArrayStrings/JavaScript/productExceptSelf.js
@@ -4,7 +4,7 @@
 
 // You must write an algorithm that runs in O(n) time and without using the division operation.
 
- // Example 1:
+// Example 1:
 
 // Input: nums = [1,2,3,4]
 // Output: [24,12,8,6]
@@ -12,16 +12,16 @@
 
 // Input: nums = [-1,1,0,-3,3]
 // Output: [0,0,9,0,0]
- 
+
 // Constraints:
 
 // 2 <= nums.length <= 105
 // -30 <= nums[i] <= 30
 // The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
- 
+
 // Follow up: Can you solve the problem in O(1) extra space complexity? (The output array does not count as extra space for space complexity analysis.)
 
-// TC: O(n), We have 3 for loops
+// TC: O(n), We loop through all the input
 // SC: O(1), we're not creating new memory
 /**
  * @param {number[]} nums

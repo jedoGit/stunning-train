f3e815987d9cca9976f2f51168e351a63a2cdaab
jedoGit <ajvillardo@gmail.com>
2024-12-18 11:04:32 -0500

    Full Text Justify

--- LeetCode/ArrayStrings/JavaScript/fullTextJustify.js
+++ LeetCode/ArrayStrings/JavaScript/fullTextJustify.js
@@ -59,18 +59,24 @@
  * @return {string[]}
  */
 var fullJustify = function (words, maxWidth) {
-  let res = [];
-  let line = [];
-  let length = 0;
-  let i = 0;
+  let res = []; // This will hold our results
+  let line = []; // This is the current line of the justified words we are currently processing
+  let length = 0; // This is the length of the words of the current line we're processing without spaces
+  let i = 0; // This is our index to each words in the string array
 
+  // We want to loop through each words in the string array
   while (i < words.length) {
+    // Check if the length of the current line we're processing, the length of the current word we're processing
+    // and the length of incoming word we're about to process will fit on the max width for each line
+    // If so, we need to process the spaces between words
     if (length + line.length + words.at(i).length > maxWidth) {
       // Line Complete
-      let extra_space = maxWidth - length;
-      let spaces = (extra_space / Math.max(1, line.length - 1)) | 0; // Integer division
-      let remainder = extra_space % Math.max(1, line.length - 1);
+      // Calculate the extra spaces we need
+      let extra_space = maxWidth - length; // Compute the space remaining after we added the current word on the the current line
+      let spaces = (extra_space / Math.max(1, line.length - 1)) | 0; // Integer division.... This computes the spaces in between the words in the current line
+      let remainder = extra_space % Math.max(1, line.length - 1); // If we don't have an even number of extra spaces computed, we need to compute the remainder and distribute the spaces starting from left.
 
+      // Add the spaces between the words in the current line
       for (let j = 0; j < Math.max(1, line.length - 1); j++) {
         line[j] += " ".repeat(spaces);
         if (remainder) {
@@ -78,18 +84,23 @@ var fullJustify = function (words, maxWidth) {
           remainder -= 1;
         }
       }
+      // We convert the current line to string then push to our results array
       res.push(line.join(""));
-      // Reset these vars
+      // Reset these vars before processing the next line
       line = [];
       length = 0;
     }
+    // push the words to the current line for processing... without spaces.
     line.push(words[i]);
     length += words[i].length;
     i += 1;
   }
   // Handling last line
+  // The last line need to be left justified... convert it to string by adding spaces between the words
+  // Also, we need to add trailing spaces
   let last_line = line.join(" ");
   let trail_space = maxWidth - last_line.length;
   res.push(last_line + " ".repeat(trail_space));
+
   return res;
 };

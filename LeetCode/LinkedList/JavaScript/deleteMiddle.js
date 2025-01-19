0b15c1984b057b25de0435866ae0a53d73f6db71
jedoGit <ajvillardo@gmail.com>
2025-01-19 08:25:40 -0500

    Delete Middle LL

--- LeetCode/LinkedList/JavaScript/deleteMiddle.js
+++ LeetCode/LinkedList/JavaScript/deleteMiddle.js
@@ -69,16 +69,3 @@ var deleteMiddle = function (head) {
   // Return the head
   return head;
 };
-
-// Recursive solution
-
-var reverseList = function (head) {
-  if (!head || !head.next) {
-    return head;
-  }
-
-  var newHead = reverseList(head.next);
-  head.next.next = head;
-  head.next = null;
-  return newHead;
-};

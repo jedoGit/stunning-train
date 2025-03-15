36f14e6493e7000769de06a8b852e1760ac64c2d
jedoGit <ajvillardo@gmail.com>
2025-03-15 09:16:39 -0400

    Kth Smallest Element in a BST

---
+++ LeetCode/BST/python/kthSmallestElementBST.py
@@ -0,0 +1,51 @@
+# Given the root of a binary search tree, and an integer k, return the kth smallest value (1-indexed) of all the values of the nodes in the tree.
+
+# Example 1:
+
+# Input: root = [3,1,4,null,2], k = 1
+# Output: 1
+# Example 2:
+
+# Input: root = [5,3,6,2,4,null,null,1], k = 3
+# Output: 3
+
+# Constraints:
+
+# The number of nodes in the tree is n.
+# 1 <= k <= n <= 104
+# 0 <= Node.val <= 104
+
+# Follow up: If the BST is modified often (i.e., we can do insert and delete operations) and you need to find the kth smallest frequently, how would you optimize?
+
+# TC: O(n)
+# SC: O(h), h is height of the tree
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        n = 0
+        s = []
+        cur = root
+
+        while cur or len(s):
+            # add all the left child to the stack
+            while cur:
+                s.append(cur)
+                cur = cur.left
+            
+            # pop the stack and increment the n counter
+            cur = s.pop()
+
+            n += 1
+
+            if n == k:
+                return cur.val
+
+            # check the right child of cur
+            cur = cur.right
+        

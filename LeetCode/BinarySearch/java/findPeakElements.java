58861fd2f3542b6cef8379d12a1e470a28d63c2d
jedoGit <ajvillardo@gmail.com>
2025-04-21 06:30:57 -0400

    Find Peak Elements

---
+++ LeetCode/BinarySearch/java/findPeakElements.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int l = 0;
+        int r = nums.length - 1;
+
+        while (l < r) {
+            int m = (int) Math.floor((r + l) / 2);
+
+            if (nums[m] > nums[m + 1]) {
+                r = m;
+            } else {
+                l = m + 1;
+            }
+        }
+        return l;
+    }
+}

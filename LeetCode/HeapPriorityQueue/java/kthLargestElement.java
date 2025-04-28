89a81e92f1133e13d6ceacd49d57c1f27b9270e7
jedoGit <ajvillardo@gmail.com>
2025-04-28 05:38:58 -0400

    Kth Largest Element

--- LeetCode/HeapPriorityQueue/java/kthLargestElement.java
+++ LeetCode/HeapPriorityQueue/java/kthLargestElement.java
@@ -11,35 +11,43 @@ class kthLargestElement {
         PriorityQueue<Integer> hMin = new PriorityQueue<>();
 
         // List<Integer> list = new ArrayList<>();
-       
+
         // for( int i = 0 ; i < nums.length ; i++ ){
-        //     list.add(nums[i]);
+        // list.add(nums[i]);
         // }
 
         List<Integer> list = Arrays.stream(nums)
-            .boxed()
-            .collect(Collectors.toList());
+                .boxed()
+                .collect(Collectors.toList());
 
         list.subList(0, k).stream()
-            .forEach(n->hMin.add(n));
+                .forEach(n -> hMin.add(n));
 
-        list.subList(k,nums.length).stream()
-            .forEach(n -> {
-                if ( n > hMin.peek() ) {
-                    hMin.poll();
-                    hMin.add(n);
-                }
-            });
+        list.subList(k, nums.length).stream()
+                .forEach(n -> {
+                    if (n > hMin.peek()) {
+                        hMin.poll();
+                        hMin.add(n);
+                    }
+                });
 
         // System.out.println(hMin);
-        
+
         return hMin.peek();
     }
 
     public static void main(String[] args) {
-        
-        int res = findKthLargest(new int[]{3,2,3,1,2,4,5,5,6}, 4);
-    
-        System.out.println("result: " + res);
+
+        int[] input1 = new int[] { 3, 2, 3, 1, 2, 4, 5, 5, 6 };
+        int k1 = 4;
+        int res1 = findKthLargest(input1, k1);
+
+        System.out.println("Input: " + Arrays.toString(input1) + ", k: " + k1 + " result " + res1);
+
+        int[] input2 = new int[] { 3, 2, 1, 5, 6, 4 };
+        int k2 = 2;
+        int res2 = findKthLargest(input2, k2);
+
+        System.out.println("Input: " + Arrays.toString(input2) + ", k: " + k2 + " result: " + res2);
     }
 }

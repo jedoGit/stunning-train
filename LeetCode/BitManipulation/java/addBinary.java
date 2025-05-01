0e6fd360c49b5187a9db5c381125856afbad78b1
jedoGit <ajvillardo@gmail.com>
2025-05-01 06:51:41 -0400

    Add Binary

--- LeetCode/BitManipulation/java/addBinary.java
+++ LeetCode/BitManipulation/java/addBinary.java
@@ -20,9 +20,10 @@ public class addBinary {
                 "1010");
         String b = String.valueOf(
                 "1011");
+        String expected = String.valueOf("10101");
 
         System.out.println(a + " + " + b + " = " + addBinaryString(a, b));
-        System.out.println("Result: " + addBinaryString(a, b).equals("10101"));
+        System.out.println("Result: " + (addBinaryString(a, b).equals(expected) ? "Correct!!!" : "Not Correct!!!"));
 
         System.out.println("=".repeat(50));
 
@@ -30,8 +31,11 @@ public class addBinary {
                 "10100000100100110110010000010101111011011001101110111111111101000000101111001110001111100001101");
         b = String.valueOf(
                 "110101001011101110001111100110001010100001101011101010000011011011001011101111001100000011011110011");
+        expected = String.valueOf(
+                "110111101100010011000101110110100000011101000101011001000011011000001100011110011010010011000000000");
 
-        System.out.println(addBinaryString(a, b));
+        System.out.println(a + " + " + b + " = " + addBinaryString(a, b));
+        System.out.println("Result: " + (addBinaryString(a, b).equals(expected) ? "Correct!!!" : "Not Correct!!!"));
 
     }
 }

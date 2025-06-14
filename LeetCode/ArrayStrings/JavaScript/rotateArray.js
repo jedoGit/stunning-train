871e7bc907771b6b27f009c0791fafae3a336260
jedoGit <jedogit@gmail.com>
2025-06-14 08:05:55 -0400

    Rotate Array

--- LeetCode/ArrayStrings/JavaScript/rotateArray.js
+++ LeetCode/ArrayStrings/JavaScript/rotateArray.js
@@ -35,7 +35,7 @@
  * @param {number} k
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var rotate = function (nums, k) {
+var rotate1 = function (nums, k) {
   k = k % nums.length;
   let n = nums.length;
 
@@ -77,7 +77,7 @@ var rotate = function (nums, k) {
  * @param {number} k
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var rotate = function (nums, k) {
+var rotate2 = function (nums, k) {
   let arr = [];
   let n = nums.length;
   k = k % n;
@@ -101,3 +101,33 @@ var rotate = function (nums, k) {
     // nums.push(...arr)
   }
 };
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @param {number[]} expected
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+let testSolution = (nums, k, expected) => {
+  console.log("Input: nums: [" + nums + "], k: " + k);
+  console.log("Expected: [" + expected + "]");
+  rotate2(nums, k);
+  console.log("Result: [" + nums + "]");
+  console.log("-".repeat(50));
+};
+
+let input = {
+  nums: [1, 2, 3, 4, 5, 6, 7],
+  k: 3,
+  expected: [5, 6, 7, 1, 2, 3, 4],
+};
+
+testSolution(input["nums"], input["k"], input["expected"]);
+
+input = {
+  nums: [-1, -100, 3, 99],
+  k: 2,
+  expected: [3, 99, -1, -100],
+};
+
+testSolution(input["nums"], input["k"], input["expected"]);

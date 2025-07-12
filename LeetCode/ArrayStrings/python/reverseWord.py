17840144b6101344574449c33dae024aa2ec16fc
jedoGit <jedogit@gmail.com>
2025-07-12 14:24:38 -0400

    Reverse Word

--- LeetCode/ArrayStrings/python/reverseWord.py
+++ LeetCode/ArrayStrings/python/reverseWord.py
@@ -2,7 +2,7 @@ import math
 import re
 
 class Solution:
-    def reverseWords(self, s: str) -> str:
+    def reverseWords1(self, s: str) -> str:
         sArray = re.split(r'\s+',s)
         # print(sArray)
         n = len(sArray)
@@ -13,14 +13,31 @@ class Solution:
             sArray[n-1-i] = tmp
 
         return " ".join(sArray).strip()
+    
+    def reverseWords2(self, s: str) -> str:
+        sArray = re.split(r'\s+',s)
+        # print(sArray)
+        n = len(sArray)
+
+        stack = []
+        revWord = ""
+
+        for word in sArray:
+            stack.append(word)
+
+        for i in range(0,n):
+            revWord = revWord + stack.pop() + " "
+
+        return revWord.strip()
 
     @staticmethod
     def testSolution(input) -> None:
         print("Input: {}".format(input["s"]))
         print("Expected: {}".format(input["expected"]))
-        s1 = Solution().reverseWords(input["s"])
-        print("Result: {}".format(s1))
-        print("Result: {}".format("Correct" if s1 == input["expected"] else "Wrong"))
+        s1 = Solution().reverseWords1(input["s"])
+        s2 = Solution().reverseWords2(input["s"])
+        print("Result1: {}, {}".format(s1, ("Correct" if s1 == input["expected"] else "Wrong")))
+        print("Result2: {}, {}".format(s2, ("Correct" if s2 == input["expected"] else "Wrong")))        
         print("-" * 50)
 
 if __name__ == "__main__":

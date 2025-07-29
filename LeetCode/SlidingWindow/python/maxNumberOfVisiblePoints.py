92da74dc933998ec2bd32f548bead7939e96fbf6
jedoGit <jedogit@gmail.com>
2025-07-29 19:45:40 -0400

    Max Numbers of Visible Points

--- LeetCode/SlidingWindow/python/maxNumberOfVisiblePoints.py
+++ LeetCode/SlidingWindow/python/maxNumberOfVisiblePoints.py
@@ -1,3 +1,7 @@
+import math
+from typing import List
+
+
 class Solution:
     def visiblePoints(self, points: List[List[int]], angle: int, location: List[int]) -> int:
         candidateAngles = []
@@ -30,3 +34,27 @@ class Solution:
             r += 1
         
         return res + onCenterCount
+    
+    @staticmethod
+    def testSolution(input) -> None:
+        print("Input: points: {}".format(input["points"]))
+        print("Input: angle: {}".format(input["angle"]))        
+        print("Input: location: {}".format(input["location"]))
+        print("Expected: {}".format(input["expected"]))
+        val = Solution().visiblePoints(input["points"], input["angle"], input["location"])
+        print("Result: {}".format(val) )
+        print( "PASS" if val == input["expected"] else "FAIL")
+        print("-" * 50)
+    
+
+if __name__ == "__main__":
+    input = {"points": [[2,1],[2,2],[3,3]], "angle": 90, "location": [1,1], "expected": 3}
+    Solution.testSolution(input)
+
+    input = {"points": [[2,1],[2,2],[3,4],[1,1]], "angle": 90, "location": [1,1], "expected": 4}
+    Solution.testSolution(input)
+
+    input = {"points": [[1,0],[2,1]], "angle": 13, "location": [1,1], "expected": 1}
+    Solution.testSolution(input)
+
+    

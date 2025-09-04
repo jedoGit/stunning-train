ed2f9623390515341e508dc93a6adc7eb3e44c60
jedoGit <jedogit@gmail.com>
2025-09-04 19:09:41 -0400

    Merge Sort

--- LeetCode/LinkedList/python/mergeSortList.py
+++ LeetCode/LinkedList/python/mergeSortList.py
@@ -26,6 +26,13 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+from typing import Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
 class Solution:
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if not head or  not head.next:

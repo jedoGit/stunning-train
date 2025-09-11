fd3baeccd8a946ad68198cce3a7d9d2de6cc3360
jedoGit <jedogit@gmail.com>
2025-09-11 18:50:03 -0400

    Reverse List II

---
+++ LeetCode/LinkedList/java/reverseListII.java
@@ -0,0 +1,144 @@
+package LeetCode.LinkedList.java;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Definition for singly-linked list.
+ **/
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+record reverseListIIRecord(int[] headVals, int left, int right, int[] expectedVals) {
+}
+
+public class reverseListII {
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        ListNode dummy = new ListNode(0, head);
+        ListNode leftPrev = dummy;
+        ListNode cur = head;
+
+        for (int i = 0; i < left - 1; i++) {
+            leftPrev = cur;
+            cur = cur.next;
+        }
+
+        ListNode prev = null;
+
+        for (int i = 0; i < right - left + 1; i++) {
+            ListNode tmpNext = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = tmpNext;
+        }
+
+        leftPrev.next.next = cur;
+        leftPrev.next = prev;
+
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        reverseListIIRecord input = new reverseListIIRecord(
+                new int[] { 1, 2, 3, 4, 5 }, 2, 4, new int[] { 1, 4, 3, 2, 5 });
+        reverseListII.testSolution(input);
+
+        input = new reverseListIIRecord(
+                new int[] { 5 }, 1, 1, new int[] { 5 });
+        reverseListII.testSolution(input);
+    }
+
+    private static void testSolution(reverseListIIRecord input) {
+        ListNode l1 = createLinkedList(input.headVals());
+        ListNode expected = createLinkedList(input.expectedVals());
+
+        System.out.println("Input: head vals: " + linkedListValueToString(l1));
+        System.out.println("Expected vals: " + linkedListValueToString(expected));
+
+        ListNode res = new reverseListII().reverseBetween(l1, input.left(), input.right());
+        System.out.println("Result: " + linkedListValueToString(res));
+        System.out.println(validateResults(res, expected) ? "PASS" : "FAIL");
+        System.out.println("-".repeat(50));
+    }
+
+    private static boolean validateResults(ListNode res, ListNode expected) {
+        if (res == null && expected == null) {
+            return true;
+        }
+
+        List<Integer> res_list = new ArrayList<>();
+        List<Integer> expected_list = new ArrayList<>();
+
+        while (res != null) {
+            res_list.add(res.val);
+            res = res.next;
+        }
+
+        while (expected != null) {
+            expected_list.add(expected.val);
+            expected = expected.next;
+        }
+
+        return res_list.equals(expected_list);
+    }
+
+    private static String linkedListValueToString(ListNode ll) {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("[ ");
+
+        while (ll != null) {
+            sb.append(ll.val);
+            sb.append(", ");
+
+            ll = ll.next;
+        }
+        // Let's remove the last comma added
+        if (sb.length() > 0) {
+            sb.setLength(sb.length() - 2);
+        }
+
+        sb.append(" ]");
+
+        return sb.toString();
+    }
+
+    private static ListNode createLinkedList(int[] headVals) {
+        int arLen = headVals.length;
+
+        ListNode[] lNodes1 = new ListNode[arLen];
+
+        // Create the linked list and connect them
+        for (int i = 0; i < arLen; i++) {
+            lNodes1[i] = new ListNode(headVals[i]);
+
+            // If this is not the first or last node, connect the node[i-1].next to node[i]
+            if (i > 0) {
+                lNodes1[i - 1].next = lNodes1[i];
+            }
+
+            // If this is the last node, connect to null
+            if (i == arLen - 1) {
+                lNodes1[i].next = null;
+            }
+        }
+
+        // Return the head of the LL
+        return lNodes1[0];
+    }
+}

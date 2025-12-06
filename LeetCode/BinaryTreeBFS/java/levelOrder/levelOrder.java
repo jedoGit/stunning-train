54f654b028d1ab881b60465b2d74a5baacdd4615
jedoGit <jedogit@gmail.com>
2025-12-06 08:40:30 -0500

    Level Order

---
+++ LeetCode/BinaryTreeBFS/java/levelOrder/levelOrder.java
@@ -0,0 +1,209 @@
+package LeetCode.BinaryTreeBFS.java.levelOrder;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Definition for a binary tree node.
+ **/
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+record levelOrderRecord(String[] root, List<List<Integer>> expected) {
+}
+
+class levelOrder {
+    public List<List<Integer>> levelOrderSolution(TreeNode root) {
+        if (root == null) {
+            return List.of();
+        }
+
+        Deque<TreeNode> q = new ArrayDeque<>();
+        q.addLast(root);
+
+        List<List<Integer>> res = new ArrayList<>();
+
+        while (!q.isEmpty()) {
+            int qLen = q.size();
+
+            List<Integer> val = new ArrayList<>();
+
+            for (int i = 0; i < qLen; i++) {
+                TreeNode node = q.removeFirst();
+                val.add(node.val);
+
+                if (node.left != null) {
+                    q.addLast(node.left);
+                }
+
+                if (node.right != null) {
+                    q.addLast(node.right);
+                }
+            }
+
+            res.add(val);
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        levelOrderRecord[] records = new levelOrderRecord[] {
+                new levelOrderRecord(
+                        new String[] { "3", "9", "20", "null", "null", "15", "7" },
+                        List.of(List.of(3), List.of(9, 20), List.of(15, 7))),
+                new levelOrderRecord(
+                        new String[] { "1" }, List.of(List.of(1))),
+                new levelOrderRecord(new String[] {}, List.of()),
+        };
+
+        int i = 1;
+        for (levelOrderRecord record : records) {
+            System.out.println("Test Case " + i++);
+            levelOrder.testSolution(record);
+        }
+    }
+
+    private static void testSolution(levelOrderRecord input) {
+        // Create the binary tree from the input array using BFS
+        TreeNode rootBTree = levelOrder.BFSCreateBinaryTree(input.root());
+
+        // Print the binary tree and the expected output
+        System.out.println("Input: root: " + levelOrder.BFSBinaryTreeToStr(rootBTree));
+        System.out.println("Expected: " + input.expected());
+
+        // Call the function to be tested
+        List<List<Integer>> res = new levelOrder().levelOrderSolution(rootBTree);
+
+        // Print the result
+        System.out.println("Result: " + res.toString());
+
+        // Validate the result
+        System.out.println(res.equals(input.expected()) ? "\u001B[32mPASS\u001B[0m" : "\u001B[31mFAIL\u001B[0m");
+        System.out.println("-".repeat(50));
+    }
+
+    private static TreeNode BFSCreateBinaryTree(String[] nodeValsArray) {
+        int arLen = nodeValsArray.length;
+
+        if (arLen < 1) {
+            return null;
+        }
+
+        TreeNode root = new TreeNode(Integer.parseInt(nodeValsArray[0]), null, null);
+        int i = 1;
+
+        // Using queue (BFS) to build the tree level by level
+        Deque<TreeNode> queue = new ArrayDeque<>();
+        queue.addLast(root);
+
+        while (!queue.isEmpty() && i < arLen) {
+            TreeNode curNode = queue.removeFirst();
+
+            if (curNode != null) {
+                // left child
+                if (i < arLen && nodeValsArray[i] != "null") {
+                    curNode.left = new TreeNode(Integer.parseInt(nodeValsArray[i]), null, null);
+                    queue.addLast(curNode.left);
+                }
+                i++;
+
+                // right child
+                if (i < arLen && nodeValsArray[i] != "null") {
+                    curNode.right = new TreeNode(Integer.parseInt(nodeValsArray[i]), null, null);
+                    queue.addLast(curNode.right);
+                }
+                i++;
+            }
+        }
+
+        return root;
+    }
+
+    private static String BFSBinaryTreeToStr(TreeNode root) {
+        // Using BFS traversal to print tree value level by level
+        if (root == null) {
+            return "[]";
+        }
+
+        List<String> sb = new ArrayList<>();
+
+        Deque<TreeNode> queue = new LinkedList<>(); // Use linkedList to allow addition of null object. ArrayDeque does
+                                                    // not allow null object
+        queue.addLast(root);
+
+        while (!queue.isEmpty()) {
+            TreeNode curNode = queue.removeFirst();
+            sb.add(curNode != null ? String.valueOf(curNode.val) : "null");
+
+            if (curNode != null) {
+                queue.addLast(curNode.left);
+                queue.addLast(curNode.right);
+            }
+        }
+
+        while (!sb.isEmpty()) {
+            if (sb.getLast() == "null") {
+                sb.removeLast();
+            } else {
+                break;
+            }
+        }
+
+        return sb.toString();
+    }
+
+    // private static boolean validateResult(TreeNode res, TreeNode expectedTree) {
+    // List<String> resList = new ArrayList<>();
+    // List<String> expectedList = new ArrayList<>();
+
+    // // Use linkedList to allow addition of null object. ArrayDeque does
+    // Deque<TreeNode> qRes = new LinkedList<>();
+    // qRes.addLast(res);
+
+    // Deque<TreeNode> qExpected = new LinkedList<>();
+    // qExpected.addLast(expectedTree);
+
+    // while (!qRes.isEmpty()) {
+    // TreeNode curNode = qRes.pollFirst();
+
+    // resList.add(curNode != null ? String.valueOf(curNode.val) : "null");
+
+    // if (curNode != null) {
+    // qRes.addLast(curNode.left);
+    // qRes.addLast(curNode.right);
+    // }
+    // }
+
+    // while (!qExpected.isEmpty()) {
+    // TreeNode curNode = qExpected.pollFirst();
+
+    // expectedList.add(curNode != null ? String.valueOf(curNode.val) : "null");
+
+    // if (curNode != null) {
+    // qExpected.addLast(curNode.left);
+    // qExpected.addLast(curNode.right);
+    // }
+    // }
+
+    // return expectedList.equals(resList);
+    // }
+}

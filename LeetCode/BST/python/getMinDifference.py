3ccbdc54b9b005bda709ac5b990a8f5012ada7ff
jedoGit <jedogit@gmail.com>
2025-12-11 07:34:02 -0500

    Minimum ABS Difference in BST

--- LeetCode/BST/python/getMinDifference.py
+++ LeetCode/BST/python/getMinDifference.py
@@ -26,31 +26,133 @@
 # SC: O(h), h is the height of the tree
 
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+from collections import deque
+from typing import Dict, List, Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
 class Solution:
+
+    def __init__(self):
+        self.min_distance = [float('inf')]
+        self.prev = [None]
+
     def getMinimumDifference(self, root: Optional[TreeNode]) -> int:
         # Binary Search Tree
 
-        min_distance = [float('inf')]
-        prev = [None]
+        # Call recursive DFS
+        self.dfs(root)
 
-        def dfs(node):
-            if node is None:
-                return
+        return self.min_distance[0]
+    
+    def dfs(self, node: Optional[TreeNode]) -> None:
+        if node is None:
+            return
 
-            dfs(node.left)
+        self.dfs(node.left)
 
-            if prev[0] is not None:
-                min_distance[0] = min(min_distance[0], node.val - prev[0])
+        if self.prev[0] is not None:
+            self.min_distance[0] = min(self.min_distance[0], node.val - self.prev[0])
 
-            prev[0] = node.val
+        self.prev[0] = node.val
 
-            dfs(node.right)
+        self.dfs(node.right)
+    
+    @staticmethod
+    def testSolution(input: Dict[str, List[str|int]]) -> None:
+        bTree = Solution.BFSCreateBinaryTree(input["root"])
+        print(f"Input: {Solution.BFSBinaryTreeToStr(bTree)}")
+        print(f"Expected: {input['expected']}")
 
-        dfs(root)
+        res = Solution().getMinimumDifference(bTree)
 
-        return min_distance[0]
+        print(f"Result: {res}")
+        print("\033[92mPASS\033[00m" if res == input.get("expected", [[]]) else "\033[91mFAIL\033[00m")
+
+    @staticmethod
+    def BFSCreateBinaryTree(node: List[int|str]) -> TreeNode:
+        arLen = len(node)
+
+        if arLen < 1:
+            return None
+
+        root = TreeNode(node[0], None, None)
+        i = 1
+
+        # Using queue (BFS) to build the tree level by level
+        queue = deque([root])
+
+        while len(queue) > 0 and i < arLen:
+            # Pre order: root, left, then right
+            cur_node = queue.popleft()
+
+            # Left child
+            if i < arLen and node[i] != "null":
+                cur_node.left = TreeNode(node[i], None, None)
+                queue.append(cur_node.left)
+            i += 1
+            
+            # Right child
+            if i < arLen and node[i] != "null":
+                cur_node.right = TreeNode(node[i], None, None)
+                queue.append(cur_node.right)            
+            i += 1
+
+        return root
+    
+    @staticmethod
+    def BFSBinaryTreeToStr(root: Optional[TreeNode]) -> None:
+
+        if root is None:
+            return "[]"
+        
+        sb = []
+
+        # Using queue (BFS) to build the tree level by level
+        queue = deque([root])
+
+
+        while len(queue) > 0:
+            curNode = queue.popleft()
+
+            tmp = str(curNode.val) if curNode is not None else "null"
+
+            sb.append(tmp)
+ 
+            if curNode is not None:
+                queue.append(curNode.left)
+                queue.append(curNode.right)
+
+        new_len = 0
+
+        # Check starting from end of sb_array where the value is not "null"
+        for i in range(len(sb) - 1, -1, -1):
+            if sb[i] == "null":
+                continue
+            else:
+                new_len = i
+                break
+
+        # Remove those extra "null"
+        if len(sb) > 0:
+            sb = sb[0 : new_len + 1]
+
+        return "[" + ", ".join(sb) + "]"
+
+
+if __name__ == "__main__":
+    # Example test cases
+    test_cases = [
+        {"root": [4,2,6,1,3], "expected": 1},
+        {"root": [1,0,48,"null","null",12,49], "expected": 1},
+    ]
+
+    for i, test_case in enumerate(test_cases):
+        print(f"Test Case {i+1}:")
+        Solution.testSolution(test_case)
+        print(f"{'-'*30}")

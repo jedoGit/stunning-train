da70429295a5e911497c11a4101a3a499932fc98
jedoGit <jedogit@gmail.com>
2025-12-20 09:35:57 -0500

    Valid BST

--- LeetCode/BST/python/isValidBST.py
+++ LeetCode/BST/python/isValidBST.py
@@ -29,11 +29,15 @@
 # SC: O(h), height of tree
 
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+from collections import deque
+from typing import Dict, List, Optional
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
         def dfs(node, left, right):
@@ -47,3 +51,96 @@ class Solution:
                     dfs(node.right, node.val, right))
 
         return dfs(root, float("-inf"), float("inf") )
+    
+    @staticmethod
+    def testSolution(input: Dict[str, List[str|int]]) -> None:
+        bTree = Solution.BFSCreateBinaryTree(input["root"])
+        print(f"Input: root: {Solution.BFSBinaryTreeToStr(bTree)}")
+        print(f"Expected: {input['expected']}")
+
+        res = Solution().isValidBST(bTree)
+
+        print(f"Result: {res}")
+        print("\033[92mPASS\033[00m" if res == input.get("expected", [[]]) else "\033[91mFAIL\033[00m")
+
+    @staticmethod
+    def BFSCreateBinaryTree(node: List[int|str]) -> TreeNode:
+        arLen = len(node)
+
+        if arLen < 1:
+            return None
+
+        root = TreeNode(node[0], None, None)
+        i = 1
+
+        # Using queue (BFS) to build the tree level by level
+        queue = deque([root])
+
+        while len(queue) > 0 and i < arLen:
+            # Pre order: root, left, then right
+            cur_node = queue.popleft()
+
+            # Left child
+            if i < arLen and node[i] != "null":
+                cur_node.left = TreeNode(node[i], None, None)
+                queue.append(cur_node.left)
+            i += 1
+            
+            # Right child
+            if i < arLen and node[i] != "null":
+                cur_node.right = TreeNode(node[i], None, None)
+                queue.append(cur_node.right)            
+            i += 1
+
+        return root
+    
+    @staticmethod
+    def BFSBinaryTreeToStr(root: Optional[TreeNode]) -> None:
+
+        if root is None:
+            return "[]"
+        
+        sb = []
+
+        # Using queue (BFS) to build the tree level by level
+        queue = deque([root])
+
+
+        while len(queue) > 0:
+            curNode = queue.popleft()
+
+            tmp = str(curNode.val) if curNode is not None else "null"
+
+            sb.append(tmp)
+ 
+            if curNode is not None:
+                queue.append(curNode.left)
+                queue.append(curNode.right)
+
+        new_len = 0
+
+        # Check starting from end of sb_array where the value is not "null"
+        for i in range(len(sb) - 1, -1, -1):
+            if sb[i] == "null":
+                continue
+            else:
+                new_len = i
+                break
+
+        # Remove those extra "null"
+        if len(sb) > 0:
+            sb = sb[0 : new_len + 1]
+
+        return "[" + ", ".join(sb) + "]"
+
+if __name__ == "__main__":
+    # Example test cases
+    test_cases = [
+        {"root": [2,1,3], "expected": True},
+        {"root": [5,1,4,"null","null",3,6], "expected": False},
+    ]
+
+    for i, test_case in enumerate(test_cases):
+        print(f"Test Case {i+1}:")
+        Solution.testSolution(test_case)
+        print(f"{'-'*30}")      

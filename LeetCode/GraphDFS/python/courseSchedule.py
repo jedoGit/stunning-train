5a782c07604132d141827284409f32a489a6057c
jedoGit <jedogit@gmail.com>
2026-01-05 07:50:03 -0500

    Course Schedule

--- LeetCode/GraphDFS/python/courseSchedule.py
+++ LeetCode/GraphDFS/python/courseSchedule.py
@@ -27,39 +27,72 @@
 # TC: O(edge + Node(vertex)) - each Node(vertex) have edges going in and going out of each nodes, it's possible that you'll visit each nodes multiple times.
 # SC: O(edge + Node(vertex)) - each Node(vertex) have edges going in and going out of each nodes, it's possible that you'll visit each nodes multiple times.
 
+from enum import Enum
+from typing import Dict, List
+
+class Result(Enum):
+    PASS = "\033[92mPASS\033[00m"
+    FAIL = "\033[91mFAIL\033[00m"
+
 class Solution:
+
+    def __init__(self):
+        self.premap = None
+        self.visited = set()
+
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         # Create an adjacency list of courses and prereqs
         # object comprehension. Create a k/v pair where k is the course number and the value is an empty array
-        premap = { c:[] for c in range(numCourses) }
+        self.premap = { c:[] for c in range(numCourses) }
         
         # prerequisites is an array of arrays [[c,p],...,[c,p]]
         # we want: premap{1:[2,3]...}
         for c,p in prerequisites:
-            premap[c].append(p)
+            self.premap[c].append(p)
 
-        visited = set()
-
-        # Helper function, DFS all courses in the premap. Returns false if course is in the visited set, true otherwise. Add course to visited set.
-        def dfs(crs):
-            if crs in visited: return False
-            if len(premap[crs]) == 0: return True # premap[crs] returns an array. We check if the size of the array is 0. If zero, there's nothing to DFS.
-
-            visited.add(crs)
-
-            # DFS the courses
-            for pre in premap[crs]:
-                if not dfs(pre):
-                    return False # We've visited this prerequisite and let's not visit it again... just return false
-
-            # Let's remove course from visited set and empty the adjacency list for this course
-            visited.remove(crs)
-            premap[crs] = []
-
-            return True
-        
         # Let's DFS all course
         for c in range(numCourses):
-            if not dfs(c): return False # We've visited this course before so, let's not continue
+            if not self.DFS(c): return False # We've visited this course before so, let's not continue
         
         return True
+
+    # Helper function, DFS all courses in the premap. Returns false if course is in the visited set, true otherwise. Add course to visited set.
+    def DFS(self, crs: int) -> bool:
+        if crs in self.visited: 
+            return False
+        
+        if len(self.premap[crs]) == 0: 
+            return True # premap[crs] returns an array. We check if the size of the array is 0. If zero, there's nothing to DFS.
+
+        self.visited.add(crs)
+
+        # DFS the courses
+        for pre in self.premap[crs]:
+            if not self.DFS(pre):
+                return False # We've visited this prerequisite and let's not visit it again... just return false
+
+        # Let's remove course from visited set and empty the adjacency list for this course
+        self.visited.remove(crs)
+        self.premap[crs] = []
+
+        return True
+
+    @staticmethod
+    def testSolution(record: Dict[str, List[List[int]]] | bool) -> None:
+        print(f"Input: numCourse: {record.get("numCourses")}")
+        print(f"prerequisites: {record.get("prerequisites")}")
+        print(f"expected: {record.get("expected")}")
+
+        res = Solution().canFinish(record["numCourses"], record["prerequisites"])
+        print(f"result: {res}")
+        print(f"{Result.PASS.value if res == record.get("expected") else Result.FAIL.value}")
+
+if __name__ == "__main__":
+    records = [ {"numCourses": 2, "prerequisites": [[1,0]], "expected": True},
+                {"numCourses": 2, "prerequisites": [[1,0],[0,1]], "expected": False}
+              ]
+    
+    for i, record in enumerate(records):
+        print(f"Test case {i+1}")
+        Solution.testSolution(record)
+        print(f"{'-' * 50}")

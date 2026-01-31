4ae000f963b93ec773a31a04d69a2b4d2a273ff3
jedoGit <jedogit@gmail.com>
2026-01-31 08:14:11 -0500

    Combinations

--- LeetCode/Backtracking/python/combinations.py
+++ LeetCode/Backtracking/python/combinations.py
@@ -22,22 +22,61 @@
 # TC:
 # SC:
 
+from dataclasses import dataclass
+from enum import Enum
+from typing import List
+
+class Result(Enum):
+    PASS = "\033[92mPASS\033[00m"
+    FAIL = "\033[91mFAIL\033[00m"
+
+@dataclass
+class combinationsRecord:
+    n: int
+    k: int
+    expected: List[List[int]]
+
 class Solution:
+    def __init__(self):
+        self.res = []
+        self.comb = []
+
     def combine(self, n: int, k: int) -> List[List[int]]:
-        res = []
-        comb = []
+        self.backtrack(1, n, k)
 
-        def backtrack(start):
-            if len(comb) == k:
-                res.append(comb[:])
-                return
+        return self.res
+    
+    def backtrack(self, start: int, n: int, k: int) -> None:
+        if len(self.comb) == k:
+            self.res.append(self.comb[:])
+            return
             
-            for i in range(start, n+1):
-                comb.append(i)
-                backtrack(i+1)
-                comb.pop()
-        
-        backtrack(1)
+        for i in range(start, n + 1):
+            self.comb.append(i)
+            self.backtrack(i + 1, n, k)
+            self.comb.pop()
+    
+    @staticmethod
+    def testSolution(record: combinationsRecord) -> None:
+        print(f"input:\tn: {record.n}")
+        print(f"\tk: {record.k}")
+        print(f"expected: {record.expected}")
 
-        return res
-        
+        res: List[List[int]] = Solution().combine(record.n, record.k)
+
+        print(f"result: {res}")
+        print(Result.PASS.value if res == record.expected else Result.FAIL.value)
+    
+if __name__ == "__main__":
+
+    records: List[combinationsRecord] = [
+        combinationsRecord(4, 2, [[1,2],[1,3],[1,4],[2,3],[2,4],[3,4]]),
+        combinationsRecord(1, 1, [[1]])
+    ]
+
+    print("-" * 50)
+    for i, record in enumerate(records):
+        print(f"# Test case {i + 1}")
+        Solution.testSolution(record)
+        print("-" * 50)
+

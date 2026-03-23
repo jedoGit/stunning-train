47ca06069381c26f21e6a4aafc82ded0c623ff3f
jedoGit <jedogit@gmail.com>
2026-03-23 07:15:31 -0400

    Add Binary

--- LeetCode/BitManipulation/java/addBinary.java
+++ LeetCode/BitManipulation/java/addBinary.java
@@ -2,9 +2,27 @@ package LeetCode.BitManipulation.java;
 
 import java.math.BigInteger;
 
-public class addBinary {
+enum testResult {
+    PASS("\u001B[32mPASS\u001B[0m"),
+    FAIL("\u001B[31mFAIL\u001B[0m");
 
-    public static String addBinaryString(String a, String b) {
+    private final String value;
+
+    testResult(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return this.value;
+    }
+}
+
+record addBinaryRecord(String a, String b, String expected) {
+}
+
+class addBinary {
+
+    public String addBinaryString1(String a, String b) {
 
         BigInteger num1 = new BigInteger(a, 2);
         BigInteger num2 = new BigInteger(b, 2);
@@ -14,28 +32,61 @@ public class addBinary {
         return sum.toString(2);
     }
 
+    public String addBinaryString2(String a, String b) {
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
+
+        StringBuilder res = new StringBuilder();
+
+        while (i > -1 || j > -1 || carry == 1) {
+            int sum = carry;
+
+            if (i > -1) {
+                sum += a.charAt(i) - '0';
+            }
+
+            if (j > -1) {
+                sum += b.charAt(j) - '0';
+            }
+
+            i--;
+            j--;
+
+            res.append(sum % 2);
+            carry = sum / 2;
+        }
+
+        return res.reverse().toString();
+    }
+
     public static void main(String[] args) {
 
-        String a = String.valueOf(
-                "1010");
-        String b = String.valueOf(
-                "1011");
-        String expected = String.valueOf("10101");
+        addBinaryRecord[] records = new addBinaryRecord[] {
+                new addBinaryRecord("11", "1", "100"),
+                new addBinaryRecord("1010", "1011", "10101"),
+                new addBinaryRecord(
+                        "10100000100100110110010000010101111011011001101110111111111101000000101111001110001111100001101",
+                        "110101001011101110001111100110001010100001101011101010000011011011001011101111001100000011011110011",
+                        "110111101100010011000101110110100000011101000101011001000011011000001100011110011010010011000000000")
+        };
 
-        System.out.println(a + " + " + b + " = " + addBinaryString(a, b));
-        System.out.println("Result: " + (addBinaryString(a, b).equals(expected) ? "Correct!!!" : "Not Correct!!!"));
+        int i = 1;
+        for (addBinaryRecord record : records) {
+            System.out.println("# Test case " + i++);
+            addBinary.testSolution(record);
+            System.out.println("-".repeat(50));
+        }
+    }
 
-        System.out.println("=".repeat(50));
+    private static void testSolution(addBinaryRecord record) {
+        System.out.println("input:\ta: " + record.a());
+        System.out.println("\tb: " + record.b());
+        System.out.println("expected: " + record.expected());
 
-        a = String.valueOf(
-                "10100000100100110110010000010101111011011001101110111111111101000000101111001110001111100001101");
-        b = String.valueOf(
-                "110101001011101110001111100110001010100001101011101010000011011011001011101111001100000011011110011");
-        expected = String.valueOf(
-                "110111101100010011000101110110100000011101000101011001000011011000001100011110011010010011000000000");
+        String res = new addBinary().addBinaryString2(record.a(), record.b());
+        System.out.println("result: " + res);
 
-        System.out.println(a + " + " + b + " = " + addBinaryString(a, b));
-        System.out.println("Result: " + (addBinaryString(a, b).equals(expected) ? "Correct!!!" : "Not Correct!!!"));
-
+        System.out.println(res.equals(record.expected()) ? testResult.PASS.getValue() : testResult.FAIL.getValue());
     }
 }

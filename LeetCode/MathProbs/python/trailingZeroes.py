4b0be341acdd47ca94df37d6ef3a90f0cbed7092
jedoGit <jedogit@gmail.com>
2026-04-11 08:30:25 -0400

    Trailing Zeroes

--- LeetCode/MathProbs/python/trailingZeroes.py
+++ LeetCode/MathProbs/python/trailingZeroes.py
@@ -27,6 +27,24 @@
 
 # Follow up: Could you write a solution that works in logarithmic time complexity?
 
+# Trailing zeros in a factorial come from multiplying pairs of 2 and 5. 
+# Since factors of 2 are more frequent,we only need to count 
+# the number of 5 s in the prime factorization of n!.
+
+from dataclasses import dataclass
+from enum import Enum
+from typing import Tuple
+
+
+class Result(Enum):
+    PASS = "\033[92mPASS\033[00m"
+    FAIL = "\033[91mFAIL\033[00m"
+
+@dataclass
+class trailingZeroesRecord:
+    n: int
+    expected: int
+
 class Solution:
     def trailingZeroes(self, n: int) -> int:
         # number of 5's in the divisor
@@ -39,30 +57,25 @@ class Solution:
 
         return count
     
-if __name__ == "__main__":
-    obj = Solution()
+    @staticmethod
+    def testSolution(record: trailingZeroesRecord) -> None:
+        print(f"input: n: {record.n}")
+        print(f"expected: {record.expected}")
+
+        res: int = Solution().trailingZeroes(record.n)
+
+        print(f"result: {res}")
+
+        print(Result.PASS.value if res == record.expected else Result.FAIL.value)
     
-    input = {"n": 3}
-    expected = 0
-    result = obj.trailingZeroes(input["n"])
-    print("Input: ", input)
-    print("Result: ", result)
-    print("Expected: ", expected)
+if __name__ == "__main__":
+    records: Tuple[trailingZeroesRecord] = (
+        trailingZeroesRecord(3, 0),
+        trailingZeroesRecord(5, 1),
+        trailingZeroesRecord(0, 0)
+    )
 
-    print("-" * 50)
-
-    input = {"n": 5}
-    expected = 1
-    result = obj.trailingZeroes(input["n"])
-    print("Input: ", input)
-    print("Result: ", result)
-    print("Expected: ", expected)
-
-    print("-" * 50)
-
-    input = {"n": 0}
-    expected = 0
-    result = obj.trailingZeroes(input["n"])
-    print("Input: ", input)
-    print("Result: ", result)
-    print("Expected: ", expected)
+    for i, record, in enumerate(records):
+        print(f"# Test case {i + 1}")
+        Solution.testSolution(record)
+        print("-" * 50)

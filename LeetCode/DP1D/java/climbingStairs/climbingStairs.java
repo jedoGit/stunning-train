d82d9dad41899e55e5380021c28541534873481f
jedoGit <jedogit@gmail.com>
2026-04-27 06:16:11 -0400

    Climbing Stairs

--- LeetCode/DP1D/java/climbingStairs/climbingStairs.java
+++ LeetCode/DP1D/java/climbingStairs/climbingStairs.java
@@ -1,12 +1,12 @@
 package LeetCode.DP1D.java.climbingStairs;
 
-enum testResult {
+enum TestResult {
     PASS("\u001B[32mPASS\u001B[0m"),
     FAIL("\u001B[31mFAIL\u001B[0m");
 
     private final String value;
 
-    testResult(String value) {
+    TestResult(String value) {
         this.value = value;
     }
 
@@ -15,19 +15,20 @@ enum testResult {
     }
 }
 
-record climbingStairsRecord(int n, int expected) {
+record ClimbingStairsRecord(int n, int expected) {
 }
 
 class climbingStairs {
     public int computeClimbStairs(int n) {
-        if (n <= 3)
+        if (n <= 2) {
             return n;
+        }
 
-        int prev1 = 3;
-        int prev2 = 2;
+        int prev1 = 2;
+        int prev2 = 1;
         int cur = 0;
 
-        for (int i = 3; i < n; i++) {
+        for (int i = 3; i <= n; i++) {
             cur = prev1 + prev2;
             prev2 = prev1;
             prev1 = cur;
@@ -36,24 +37,28 @@ class climbingStairs {
         return cur;
     }
 
-    private static void testSolution(climbingStairsRecord record) {
+    private static void testSolution(ClimbingStairsRecord record) {
         System.out.println("input: n: " + record.n());
         System.out.println("expected: " + record.expected());
 
         int res = new climbingStairs().computeClimbStairs(record.n());
         System.out.println("result: " + res);
 
-        System.out.println(res == record.expected() ? testResult.PASS.getValue() : testResult.FAIL.getValue());
+        System.out.println(res == record.expected() ? TestResult.PASS.getValue() : TestResult.FAIL.getValue());
     }
 
     public static void main(String[] args) {
-        climbingStairsRecord[] records = new climbingStairsRecord[] {
-                new climbingStairsRecord(2, 2),
-                new climbingStairsRecord(3, 3)
+        ClimbingStairsRecord[] records = new ClimbingStairsRecord[] {
+                new ClimbingStairsRecord(1, 1),
+                new ClimbingStairsRecord(2, 2),
+                new ClimbingStairsRecord(3, 3),
+                new ClimbingStairsRecord(4, 5),
+                new ClimbingStairsRecord(5, 8),
+                new ClimbingStairsRecord(6, 13)
         };
 
         int i = 1;
-        for (climbingStairsRecord record : records) {
+        for (ClimbingStairsRecord record : records) {
             System.out.println("# Test case " + i++);
             climbingStairs.testSolution(record);
             System.out.println("-".repeat(50));

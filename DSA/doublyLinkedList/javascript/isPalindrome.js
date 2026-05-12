20cb69703d2ea5e6130370d467bac7d7a581cec2
jedogit <jedogit@gmail.com>
2026-05-12 06:05:41 -0400

    Refactor is palindrome JavaScript tests

--- DSA/doublyLinkedList/javascript/isPalindrome.js
+++ DSA/doublyLinkedList/javascript/isPalindrome.js
@@ -5,27 +5,99 @@
 // Constraints:
 // You can only traverse the doubly linked list once.
 
-function isPalindrome() {
-  if (this.length < 2) {
-    return true;
+const Result = { PASS: "\x1b[92mPASS\x1b[0m", FAIL: "\x1b[91mFAIL\x1b[0m" };
+
+class Record {
+  constructor(values, expected) {
+    this.values = values;
+    this.expected = expected;
+  }
+}
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor(values) {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+
+    values.forEach((value) => this.push(value));
   }
 
-  let start = this.head;
-  let end = this.tail;
+  push(value) {
+    const newNode = new Node(value);
 
-  // We want to compare the values of the start and end pointer
-  // For each iteration, we want to move the start pointer to the right
-  // while, we move the end pointer to the left.
-  // We'll stop once we hit this.length/2, which is the middle of the DLL.
-
-  for (let i = 0; i < Math.floor(this.length / 2); i++) {
-    if (start.value !== end.value) {
-      return false;
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
     }
 
-    start = start.next;
-    end = end.prev;
+    this.length++;
+    return this;
   }
-
-  return true;
 }
+
+class Solution {
+  isPalindrome(values) {
+    const list = new DoublyLinkedList(values);
+
+    if (list.length < 2) {
+      return true;
+    }
+
+    let start = list.head;
+    let end = list.tail;
+
+    // We want to compare the values of the start and end pointer
+    // For each iteration, we want to move the start pointer to the right
+    // while, we move the end pointer to the left.
+    // We'll stop once we hit this.length/2, which is the middle of the DLL.
+
+    for (let i = 0; i < Math.floor(list.length / 2); i++) {
+      if (start.value !== end.value) {
+        return false;
+      }
+
+      start = start.next;
+      end = end.prev;
+    }
+
+    return true;
+  }
+}
+
+function testSolution(record) {
+  const solution = new Solution();
+  const result = solution.isPalindrome(record.values);
+  const status = result === record.expected ? Result.PASS : Result.FAIL;
+
+  console.log(`Input: ${JSON.stringify(record.values)}`);
+  console.log(`Expected: ${record.expected}`);
+  console.log(`Result: ${result}`);
+  console.log(status);
+}
+
+const records = [
+  new Record([], true),
+  new Record([1], true),
+  new Record([1, 2, 1], true),
+  new Record([1, 2, 2, 1], true),
+  new Record([1, 2, 3], false),
+];
+
+records.forEach((record, index) => {
+  console.log(`# Test case ${index + 1}`);
+  testSolution(record);
+  console.log('--------------------');
+});

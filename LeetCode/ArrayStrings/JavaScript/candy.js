c87c75e10269c987b3193f6244ea4a0fbe6362b6
jedogit <jedogit@gmail.com>
2026-05-19 06:04:31 -0400

    Refactor candy JavaScript tests

--- LeetCode/ArrayStrings/JavaScript/candy.js
+++ LeetCode/ArrayStrings/JavaScript/candy.js
@@ -27,35 +27,70 @@
 // TC: O(4n) => O(n) because we have to initialize candy array of n students with 1, loop left, then right then accumulate the sum of n student ratings
 // SC: O(n) because we have to create an array to store the candies for each students
 
-/**
- * @param {number[]} ratings
- * @return {number}
- */
-var candy = function (ratings) {
-  // We need to set an candiesay to capture how many candies were given per student and initialize with zero
-  // index  0,1,2,3,4,5
-  // candies = [1,1,1,1,1,1]
-  const n = ratings.length;
-  let candies = new Array(n).fill(1);
+const Result = { PASS: "\x1b[92mPASS\x1b[0m", FAIL: "\x1b[91mFAIL\x1b[0m" };
 
-  // For the first pass, we need to loop from left to right and compare if the left rating is less than the current rating
-  // If so, the value of the current candy is the left candy plus 1
-  for (let i = 1; i < n; i++) {
-    if (ratings[i - 1] < ratings[i]) {
-      candies[i] = candies[i - 1] + 1;
-    }
+class CandyRecord {
+  constructor(ratings, expected) {
+    this.ratings = ratings;
+    this.expected = expected;
   }
+}
 
-  // The second pass, we need to loop from right to left and compare if the right rating is less than the current rating
-  // If so, we take the max of the current candy and the right candy plus 1 and assign to the current candy.
-  for (let i = n - 2; i > -1; i--) {
-    if (ratings[i] > ratings[i + 1]) {
-      candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+class Solution {
+  /**
+   * @param {number[]} ratings
+   * @return {number}
+   */
+  candy(ratings) {
+    // We need to set an candiesay to capture how many candies were given per student and initialize with zero
+    // index  0,1,2,3,4,5
+    // candies = [1,1,1,1,1,1]
+    const n = ratings.length;
+    let candies = new Array(n).fill(1);
+
+    // For the first pass, we need to loop from left to right and compare if the left rating is less than the current rating
+    // If so, the value of the current candy is the left candy plus 1
+    for (let i = 1; i < n; i++) {
+      if (ratings[i - 1] < ratings[i]) {
+        candies[i] = candies[i - 1] + 1;
+      }
     }
+
+    // The second pass, we need to loop from right to left and compare if the right rating is less than the current rating
+    // If so, we take the max of the current candy and the right candy plus 1 and assign to the current candy.
+    for (let i = n - 2; i > -1; i--) {
+      if (ratings[i] > ratings[i + 1]) {
+        candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+      }
+    }
+
+    // Finally we need to sum all values of the candies candiesay and return it
+    const ret = candies.reduce((acc, curVal) => acc + curVal, 0);
+
+    return ret;
   }
+}
 
-  // Finally we need to sum all values of the candies candiesay and return it
-  const ret = candies.reduce((acc, curVal) => acc + curVal, 0);
+function testSolution(record) {
+  const solution = new Solution();
+  const result = solution.candy([...record.ratings]);
+  const status = result === record.expected ? Result.PASS : Result.FAIL;
 
-  return ret;
-};
+  console.log(`input: ratings: ${JSON.stringify(record.ratings)}`);
+  console.log(`expected: ${record.expected}`);
+  console.log(`result: ${result}`);
+  console.log(status);
+}
+
+const records = [
+  new CandyRecord([1, 0, 2], 5),
+  new CandyRecord([1, 2, 2], 4),
+  new CandyRecord([1, 3, 4, 5, 2], 11),
+  new CandyRecord([1], 1),
+];
+
+records.forEach((record, index) => {
+  console.log(`# Test case ${index + 1}`);
+  testSolution(record);
+  console.log("-".repeat(50));
+});

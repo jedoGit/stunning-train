7d4f5cbded8027936d07f6b3f75b40c75c1c6cce
jedoGit <jedogit@gmail.com>
2026-05-25 06:01:07 -0400

    Refactor square root x JavaScript tests

--- LeetCode/MathProbs/javascript/squareRootX.js
+++ LeetCode/MathProbs/javascript/squareRootX.js
@@ -1,9 +1,13 @@
-const Result = Object.freeze({
-  PASS: "\x1b[92mPASS\x1b[0m",
-  FAIL: "\x1b[91mFAIL\x1b[0m",
-});
+const Result = { PASS: "\x1b[92mPASS\x1b[0m", FAIL: "\x1b[91mFAIL\x1b[0m" };
 
-class SquareRootX {
+class SquareRootRecord {
+  constructor(x, expected) {
+    this.x = x;
+    this.expected = expected;
+  }
+}
+
+class Solution {
   /**
    * Calculates the square root of x rounded down to the nearest integer.
    * @param {number} x
@@ -31,31 +35,27 @@ class SquareRootX {
     return res;
   }
 
-  /**
-   * Static method to test the solution
-   * @param {Object} record
-   */
-  static testSolution(record) {
-    console.log(`input: x: ${record.x}`);
-    console.log(`expected: ${record.expected}`);
-
-    const solver = new SquareRootX();
-    const res = solver.mySqrt(record.x);
-
-    console.log(`result: ${res}`);
-    console.log(res === record.expected ? Result.PASS : Result.FAIL);
-  }
 }
 
-// Main Execution
+function testSolution(record) {
+  console.log(`input: x: ${record.x}`);
+  console.log(`expected: ${record.expected}`);
+
+  const solution = new Solution();
+  const result = solution.mySqrt(record.x);
+
+  console.log(`result: ${result}`);
+  console.log(result === record.expected ? Result.PASS : Result.FAIL);
+}
+
 const records = [
-  { x: 4, expected: 2 },
-  { x: 8, expected: 2 },
-  { x: 2147395599, expected: 46339 },
+  new SquareRootRecord(4, 2),
+  new SquareRootRecord(8, 2),
+  new SquareRootRecord(2147395599, 46339),
 ];
 
 records.forEach((record, i) => {
   console.log(`# Test case ${i + 1}`);
-  SquareRootX.testSolution(record);
+  testSolution(record);
   console.log("-".repeat(50));
 });

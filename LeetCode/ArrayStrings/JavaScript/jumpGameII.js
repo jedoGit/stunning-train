53c5207f60bd359afe04bd2f7bc770a1376416f6
jedogit <jedogit@gmail.com>
2026-06-16 06:05:21 -0400

    Refactor jump game II JavaScript tests

--- LeetCode/ArrayStrings/JavaScript/jumpGameII.js
+++ LeetCode/ArrayStrings/JavaScript/jumpGameII.js
@@ -25,36 +25,71 @@
 // TC: O(n) because we loop through the elements of nums
 // SC: O(1) in place processing
 
-/**
- * @param {number[]} nums
- * @return {number}
- */
-var jump = function (nums) {
-  let n = nums.length;
-  let res = 0;
-  let l = 0;
-  let r = 0;
+const Result = { PASS: "\x1b[92mPASS\x1b[0m", FAIL: "\x1b[91mFAIL\x1b[0m" };
 
-  // We stop once the r pointer exceeds the end index
-  while (r < n - 1) {
-    // For each iteration, we reset the farthest variable.
-    // We use the farthest variable to capture the farthest jump based on the value of the element.
-    // For example, if the pointer is pointing 2, the farthest variable should point to current index plus 2.
-    let farthest = 0;
-    // Starting at the left pointer and ending at the right pointer, we check the value of the elements.
-    // Then, we update the value of farthest based on the max value we computed between this interval
-    for (let i = l; i < r + 1; i++) {
-      farthest = Math.max(farthest, i + nums[i]);
+class JumpGameIIRecord {
+  constructor(nums, expected) {
+    this.nums = nums;
+    this.expected = expected;
+  }
+}
+
+class Solution {
+  /**
+   * @param {number[]} nums
+   * @return {number}
+   */
+  jump(nums) {
+    let n = nums.length;
+    let res = 0;
+    let l = 0;
+    let r = 0;
+
+    // We stop once the r pointer exceeds the end index
+    while (r < n - 1) {
+      // For each iteration, we reset the farthest variable.
+      // We use the farthest variable to capture the farthest jump based on the value of the element.
+      // For example, if the pointer is pointing 2, the farthest variable should point to current index plus 2.
+      let farthest = 0;
+      // Starting at the left pointer and ending at the right pointer, we check the value of the elements.
+      // Then, we update the value of farthest based on the max value we computed between this interval
+      for (let i = l; i < r + 1; i++) {
+        farthest = Math.max(farthest, i + nums[i]);
+      }
+
+      // After we computed what's the farthest value from the interval, we update our l & r pointers
+      // We move the l pointer to r+1 and the r pointer to the farthest value
+      l = r + 1;
+      r = farthest;
+
+      // We increment res every iteration
+      res = res + 1;
     }
 
-    // After we computed what's the farthest value from the interval, we update our l & r pointers
-    // We move the l pointer to r+1 and the r pointer to the farthest value
-    l = r + 1;
-    r = farthest;
-
-    // We increment res every iteration
-    res = res + 1;
+    return res;
   }
+}
 
-  return res;
-};
+function testSolution(record) {
+  const solution = new Solution();
+  const result = solution.jump(record.nums);
+  const status = result === record.expected ? Result.PASS : Result.FAIL;
+
+  console.log(`Input: nums = ${JSON.stringify(record.nums)}`);
+  console.log(`Expected: ${record.expected}`);
+  console.log(`Result: ${result}`);
+  console.log(status);
+}
+
+const records = [
+  new JumpGameIIRecord([2, 3, 1, 1, 4], 2),
+  new JumpGameIIRecord([2, 3, 0, 1, 4], 2),
+  new JumpGameIIRecord([0], 0),
+  new JumpGameIIRecord([1, 2], 1),
+];
+
+records.forEach((record, index) => {
+  console.log(`# Test case ${index + 1}`);
+  testSolution(record);
+  console.log("----------------------------------------");
+});

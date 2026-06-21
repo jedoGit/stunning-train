ba50a9de2ee670af305a485d9563ee4dd1c5f1ff
jedogit <jedogit@gmail.com>
2026-06-21 06:02:39 -0400

    Refactor Binary Search Tree Iterator JavaScript tests

--- LeetCode/BinarySearch/JavaScript/BinarySearchTreeIterator.js
+++ LeetCode/BinarySearch/JavaScript/BinarySearchTreeIterator.js
@@ -48,44 +48,130 @@
 /**
  * @param {TreeNode} root
  */
-var BSTIterator = function (root) {
-  // We'll be using a stack
-  this.stack = [];
+const Result = { PASS: "\x1b[92mPASS\x1b[0m", FAIL: "\x1b[91mFAIL\x1b[0m" };
 
-  // We'll add only the root and the left child to the stack
-  // This is iterative DFS until there's no more left child
-  // This is due to the in-order traversal constraint
-  let cur = root;
-  while (cur) {
-    this.stack.push(cur);
-    cur = cur.left;
+class TreeNode {
+  constructor(val, left, right) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
   }
-};
+}
 
-/**
- * @return {number}
- */
-BSTIterator.prototype.next = function () {
-  let res = this.stack.pop();
-  // Once we pop the node added to the stack,
-  // we'll need to check if that node has a right child
-  // if it does, add the left child by iterative DFS'ing it
-  let cur = res.right;
-  while (cur) {
-    this.stack.push(cur);
-    cur = cur.left;
+class BSTIterator {
+  constructor(root) {
+    // We'll be using a stack
+    this.stack = [];
+
+    // We'll add only the root and the left child to the stack
+    // This is iterative DFS until there's no more left child
+    // This is due to the in-order traversal constraint
+    let cur = root;
+    while (cur) {
+      this.stack.push(cur);
+      cur = cur.left;
+    }
   }
 
-  // return the value of the node we just popped.
-  return res.val;
-};
+  /**
+   * @return {number}
+   */
+  next() {
+    let res = this.stack.pop();
+    // Once we pop the node added to the stack,
+    // we'll need to check if that node has a right child
+    // if it does, add the left child by iterative DFS'ing it
+    let cur = res.right;
+    while (cur) {
+      this.stack.push(cur);
+      cur = cur.left;
+    }
 
-/**
- * @return {boolean}
- */
-BSTIterator.prototype.hasNext = function () {
-  return this.stack.length !== 0;
-};
+    // return the value of the node we just popped.
+    return res.val;
+  }
+
+  /**
+   * @return {boolean}
+   */
+  hasNext() {
+    return this.stack.length !== 0;
+  }
+}
+
+class BinarySearchTreeIteratorRecord {
+  constructor(root, operations, expected) {
+    this.root = root;
+    this.operations = operations;
+    this.expected = expected;
+  }
+}
+
+class Solution {
+  bstIterator(root, operations) {
+    const iterator = new BSTIterator(root);
+    return operations.map((operation) => iterator[operation]());
+  }
+}
+
+function buildTree(values) {
+  if (!values.length || values[0] === null) {
+    return null;
+  }
+
+  const root = new TreeNode(values[0]);
+  const queue = [root];
+  let index = 1;
+
+  while (queue.length && index < values.length) {
+    const node = queue.shift();
+
+    if (values[index] !== null && values[index] !== undefined) {
+      node.left = new TreeNode(values[index]);
+      queue.push(node.left);
+    }
+    index += 1;
+
+    if (index < values.length && values[index] !== null && values[index] !== undefined) {
+      node.right = new TreeNode(values[index]);
+      queue.push(node.right);
+    }
+    index += 1;
+  }
+
+  return root;
+}
+
+function testSolution(record) {
+  const solution = new Solution();
+  const root = buildTree(record.root);
+  const result = solution.bstIterator(root, record.operations);
+  const status = JSON.stringify(result) === JSON.stringify(record.expected) ? Result.PASS : Result.FAIL;
+
+  console.log(`Input: root = ${JSON.stringify(record.root)}, operations = ${JSON.stringify(record.operations)}`);
+  console.log(`Expected: ${JSON.stringify(record.expected)}`);
+  console.log(`Result: ${JSON.stringify(result)}`);
+  console.log(status);
+}
+
+const records = [
+  new BinarySearchTreeIteratorRecord(
+    [7, 3, 15, null, null, 9, 20],
+    ["next", "next", "hasNext", "next", "hasNext", "next", "hasNext", "next", "hasNext"],
+    [3, 7, true, 9, true, 15, true, 20, false],
+  ),
+  new BinarySearchTreeIteratorRecord(
+    [3, 1, 4, null, 2],
+    ["next", "next", "next", "hasNext", "next", "hasNext"],
+    [1, 2, 3, true, 4, false],
+  ),
+];
+
+records.forEach((record, index) => {
+  console.log(`# Test case ${index + 1}`);
+  testSolution(record);
+  console.log("--------------------");
+});
 
 /**
  * Your BSTIterator object will be instantiated and called as such:

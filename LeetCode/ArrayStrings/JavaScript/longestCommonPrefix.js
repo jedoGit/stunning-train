22ba630129937ceefd3f28330b9860d249e932d5
jedogit <jedogit@gmail.com>
2026-06-27 06:02:03 -0400

    Refactor longest common prefix JavaScript tests

--- LeetCode/ArrayStrings/JavaScript/longestCommonPrefix.js
+++ LeetCode/ArrayStrings/JavaScript/longestCommonPrefix.js
@@ -21,28 +21,72 @@
 // TC: O(nlogn) worst case due to sorting, if sorting is not counted, it's O(m) which is the lenght of the string at index 0
 // SC: O(n) worst case due to sorting, if sorting is not counted, it's O(1)
 
-/**
- * @param {string[]} strs
- * @return {string}
- */
-var longestCommonPrefix = function (strs) {
-  // We need to sort the words in the strings
-  // After it's sorted, we compare the words at index 0 and the last index chars by chars
-  // Exit if we see that there's no common chars
-
-  strs.sort();
-  const n = strs.length;
-  let s1 = strs[0];
-  let s2 = strs[n - 1];
-  let res = "";
-
-  let i = 0;
-
-  // We use s1's length. We could use s2 if you want...
-  while (i < s1.length && s1[i] === s2[i]) {
-    res += s1[i];
-    i++;
-  }
-
-  return res;
+const Result = {
+  PASS: "\x1b[92mPASS\x1b[0m",
+  FAIL: "\x1b[91mFAIL\x1b[0m",
 };
+
+class LongestCommonPrefixRecord {
+  constructor(strs, expected) {
+    this.strs = strs;
+    this.expected = expected;
+  }
+}
+
+/**
+ * TC: O(nlogn) worst case due to sorting, if sorting is not counted, it's O(m)
+ * SC: O(n) worst case due to sorting, if sorting is not counted, it's O(1)
+ */
+class Solution {
+  /**
+   * @param {string[]} strs
+   * @return {string}
+   */
+  longestCommonPrefix(strs) {
+    // We need to sort the words in the strings
+    // After it's sorted, we compare the words at index 0 and the last index chars by chars
+    // Exit if we see that there's no common chars
+
+    strs.sort();
+    const n = strs.length;
+    let s1 = strs[0];
+    let s2 = strs[n - 1];
+    let res = "";
+
+    let i = 0;
+
+    // We use s1's length. We could use s2 if you want...
+    while (i < s1.length && s1[i] === s2[i]) {
+      res += s1[i];
+      i++;
+    }
+
+    return res;
+  }
+}
+
+function testSolution(record) {
+  const solution = new Solution();
+  const strs = [...record.strs];
+  const result = solution.longestCommonPrefix(strs);
+  const pass = result === record.expected;
+
+  console.log(`Input: strs = ${JSON.stringify(record.strs)}`);
+  console.log(`Expected: ${record.expected}`);
+  console.log(`Result: ${result}`);
+  console.log(pass ? Result.PASS : Result.FAIL);
+}
+
+const records = [
+  new LongestCommonPrefixRecord(["flower", "flow", "flight"], "fl"),
+  new LongestCommonPrefixRecord(["dog", "racecar", "car"], ""),
+  new LongestCommonPrefixRecord(["interspecies", "interstellar", "interstate"], "inters"),
+  new LongestCommonPrefixRecord(["throne", "throne"], "throne"),
+  new LongestCommonPrefixRecord([""], ""),
+];
+
+records.forEach((record, index) => {
+  console.log(`# Test case ${index + 1}`);
+  testSolution(record);
+  console.log("-".repeat(30));
+});

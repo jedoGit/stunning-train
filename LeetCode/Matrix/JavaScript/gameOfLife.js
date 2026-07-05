10954e0423473b93fd99523619c48f8b1c6cbc30
jedogit <jedogit@gmail.com>
2026-07-05 06:02:56 -0400

    Refactor Game of Life JavaScript tests

--- LeetCode/Matrix/JavaScript/gameOfLife.js
+++ LeetCode/Matrix/JavaScript/gameOfLife.js
@@ -36,87 +36,147 @@
 // TC: O(3*m*n) => O(mn)... We have to visit each cells 3 times to check its neighbors, to assign values and to fix the final values
 // SC: O(1)
 
-/**
- * @param {number[][]} board
- * @return {void} Do not return anything, modify board in-place instead.
- */
-var gameOfLife = function (board) {
-  // This is our state table... we'll represent our state changes like this
-  // Old      | New   |   State
-  // 0        |   0   |   0
-  // 1        |   0   |   1
-  // 0        |   1   |   2
-  // 1        |   1   |   3
+const Result = { PASS: "\x1b[92mPASS\x1b[0m", FAIL: "\x1b[91mFAIL\x1b[0m" };
 
-  // This is the number of rows and cols of the matrix
-  let rows = board.length;
-  let cols = board[0].length;
+class Record {
+  constructor(board, expected) {
+    this.board = board;
+    this.expected = expected;
+  }
+}
 
-  // This is our helper function to check the neighbor counts of the cell
-  function countNeighbors(r, c) {
-    let nei = 0;
-    // Check each elements of the matrix
-    for (let i = r - 1; i < r + 2; i++) {
-      for (let j = c - 1; j < c + 2; j++) {
-        // Continue if:
-        // 1. i,j is r,c
-        // 2. i or j is out of bounds, ie negative
-        // 3. i or j is out of bounds, ie greater than the matrix length
-        if (
-          (i === r && j === c) ||
-          i < 0 ||
-          j < 0 ||
-          i === rows ||
-          j === cols
-        ) {
-          continue;
+class Solution {
+  /**
+   * @param {number[][]} board
+   * @return {void} Do not return anything, modify board in-place instead.
+   */
+  gameOfLife(board) {
+    // This is our state table... we'll represent our state changes like this
+    // Old      | New   |   State
+    // 0        |   0   |   0
+    // 1        |   0   |   1
+    // 0        |   1   |   2
+    // 1        |   1   |   3
+
+    // This is the number of rows and cols of the matrix
+    let rows = board.length;
+    let cols = board[0].length;
+
+    // This is our helper function to check the neighbor counts of the cell
+    function countNeighbors(r, c) {
+      let nei = 0;
+      // Check each elements of the matrix
+      for (let i = r - 1; i < r + 2; i++) {
+        for (let j = c - 1; j < c + 2; j++) {
+          // Continue if:
+          // 1. i,j is r,c
+          // 2. i or j is out of bounds, ie negative
+          // 3. i or j is out of bounds, ie greater than the matrix length
+          if (
+            (i === r && j === c) ||
+            i < 0 ||
+            j < 0 ||
+            i === rows ||
+            j === cols
+          ) {
+            continue;
+          }
+          // Check if the matrix elements is equal to 1 or 3, if so,
+          // increment nei by 1. This is the neighbor count of the current cell
+          if (board[i][j] === 1 || board[i][j] === 3) {
+            nei += 1;
+          }
         }
-        // Check if the matrix elements is equal to 1 or 3, if so,
-        // increment nei by 1. This is the neighbor count of the current cell
-        if (board[i][j] === 1 || board[i][j] === 3) {
-          nei += 1;
+      }
+      return nei;
+    }
+
+    // Here, we visit each elements of the matrix and check its neighbors
+    // we use our state diagram above to assign values based on its neighbors
+    for (let r = 0; r < rows; r++) {
+      for (let c = 0; c < cols; c++) {
+        // First, get the neighbor count of the current cell
+        const nei = countNeighbors(r, c);
+        // console.log(nei)
+        // If the current cell is zero
+        // we check if it has 2 or 3 neighbors
+        if (board[r][c]) {
+          // If it has 2 or 3 neighbors, we change the value of the current cell to 3
+          // based on the state diagram
+          if (nei === 2 || nei === 3) {
+            board[r][c] = 3;
+            // console.log("board[r][c] in [2,3], set to 3")
+          }
+        } else if (nei === 3) {
+          // here, the current element is non-zero and it has 3 neighbors
+          // We assign a value of 2
+          board[r][c] = 2;
         }
       }
     }
-    return nei;
-  }
 
-  // Here, we visit each elements of the matrix and check its neighbors
-  // we use our state diagram above to assign values based on its neighbors
-  for (let r = 0; r < rows; r++) {
-    for (let c = 0; c < cols; c++) {
-      // First, get the neighbor count of the current cell
-      const nei = countNeighbors(r, c);
-      // console.log(nei)
-      // If the current cell is zero
-      // we check if it has 2 or 3 neighbors
-      if (board[r][c]) {
-        // If it has 2 or 3 neighbors, we change the value of the current cell to 3
-        // based on the state diagram
-        if (nei === 2 || nei === 3) {
-          board[r][c] = 3;
-          // console.log("board[r][c] in [2,3], set to 3")
+    // At this point, we're done assigning values to each cells based on the state diagram
+    // now, we're ready to update each cells if they're dead or not... 0 or 1
+    for (let r = 0; r < rows; r++) {
+      for (let c = 0; c < cols; c++) {
+        // If the cell was assigned a 1, we set it to zero
+        if (board[r][c] === 1) {
+          board[r][c] = 0;
+        } else if (board[r][c] === 2 || board[r][c] === 3) {
+          // if a cell is assigned a 2 or a 3, we set it to 1
+          board[r][c] = 1;
+          // console.log("board[r][c] in [2,3], set to 1")
         }
-      } else if (nei === 3) {
-        // here, the current element is non-zero and it has 3 neighbors
-        // We assign a value of 2
-        board[r][c] = 2;
       }
     }
   }
+}
 
-  // At this point, we're done assigning values to each cells based on the state diagram
-  // now, we're ready to update each cells if they're dead or not... 0 or 1
-  for (let r = 0; r < rows; r++) {
-    for (let c = 0; c < cols; c++) {
-      // If the cell was assigned a 1, we set it to zero
-      if (board[r][c] === 1) {
-        board[r][c] = 0;
-      } else if (board[r][c] === 2 || board[r][c] === 3) {
-        // if a cell is assigned a 2 or a 3, we set it to 1
-        board[r][c] = 1;
-        // console.log("board[r][c] in [2,3], set to 1")
-      }
-    }
-  }
+const cloneBoard = (board) => board.map((row) => [...row]);
+
+const testSolution = (record) => {
+  const solution = new Solution();
+  const board = cloneBoard(record.board);
+
+  solution.gameOfLife(board);
+
+  const passed = JSON.stringify(board) === JSON.stringify(record.expected);
+
+  console.log(`Input: ${JSON.stringify(record.board)}`);
+  console.log(`Expected: ${JSON.stringify(record.expected)}`);
+  console.log(`Result: ${JSON.stringify(board)}`);
+  console.log(passed ? Result.PASS : Result.FAIL);
 };
+
+const records = [
+  new Record(
+    [
+      [0, 1, 0],
+      [0, 0, 1],
+      [1, 1, 1],
+      [0, 0, 0],
+    ],
+    [
+      [0, 0, 0],
+      [1, 0, 1],
+      [0, 1, 1],
+      [0, 1, 0],
+    ]
+  ),
+  new Record(
+    [
+      [1, 1],
+      [1, 0],
+    ],
+    [
+      [1, 1],
+      [1, 1],
+    ]
+  ),
+];
+
+records.forEach((record, index) => {
+  console.log(`# Test case ${index + 1}`);
+  testSolution(record);
+  console.log("--------------------");
+});

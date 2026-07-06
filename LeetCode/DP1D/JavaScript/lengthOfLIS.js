43540b7ef9bec0e30ad1c7d26509eb5ac0fe39c1
jedogit <jedogit@gmail.com>
2026-07-06 06:36:43 -0400

    Refactor longest increasing subsequence JavaScript tests

--- LeetCode/DP1D/JavaScript/lengthOfLIS.js
+++ LeetCode/DP1D/JavaScript/lengthOfLIS.js
@@ -2,44 +2,54 @@
  * @param {number[]} nums
  * @return {number}
  */
-var lengthOfLIS = function (nums) {
-  LIS = Array(nums.length).fill(1);
+const Result = { PASS: "\x1b[92mPASS\x1b[0m", FAIL: "\x1b[91mFAIL\x1b[0m" };
 
-  for (let i = nums.length - 1; i > -1; i -= 1) {
-    // for (let j = i + 1; j < nums.length; j += 1) {
-    for (let j of Array.from(
-      { length: nums.length },
-      (_, idx) => idx + i + 1
-    )) {
-      if (nums[i] < nums[j]) {
-        LIS[i] = Math.max(LIS[i], 1 + LIS[j]);
+class LengthOfLISRecord {
+  constructor(nums, expected) {
+    this.nums = nums;
+    this.expected = expected;
+  }
+}
+
+class Solution {
+  /**
+   * @param {number[]} nums
+   * @return {number}
+   */
+  lengthOfLIS(nums) {
+    const lis = Array(nums.length).fill(1);
+
+    for (let i = nums.length - 1; i > -1; i -= 1) {
+      for (let j = i + 1; j < nums.length; j += 1) {
+        if (nums[i] < nums[j]) {
+          lis[i] = Math.max(lis[i], 1 + lis[j]);
+        }
       }
     }
+
+    return Math.max(...lis);
   }
+}
 
-  return Math.max(...LIS);
-};
+function testSolution(record) {
+  const solution = new Solution();
+  const result = solution.lengthOfLIS(record.nums);
+  const status = result === record.expected ? Result.PASS : Result.FAIL;
 
-let input1 = { nums: [10, 9, 2, 5, 3, 7, 101, 18] };
-let expected1 = 4;
-let result1 = lengthOfLIS(input1["nums"]);
-console.log("Input: [" + input1.nums + "]");
-console.log("Result: " + result1);
-console.log("Expected: " + expected1);
-console.log("-".repeat(50));
+  console.log(`Input: ${JSON.stringify(record.nums)}`);
+  console.log(`Expected: ${record.expected}`);
+  console.log(`Result: ${result}`);
+  console.log(status);
+}
 
-let input2 = { nums: [0, 1, 0, 3, 2, 3] };
-let expected2 = 4;
-let result2 = lengthOfLIS(input2["nums"]);
-console.log("Input: [" + input2.nums + "]");
-console.log("Result: " + result2);
-console.log("Expected: " + expected2);
-console.log("-".repeat(50));
+const records = [
+  new LengthOfLISRecord([10, 9, 2, 5, 3, 7, 101, 18], 4),
+  new LengthOfLISRecord([0, 1, 0, 3, 2, 3], 4),
+  new LengthOfLISRecord([7, 7, 7, 7, 7, 7, 7], 1),
+];
 
-let input3 = { nums: [7, 7, 7, 7, 7, 7, 7] };
-let expected3 = 1;
-let result3 = lengthOfLIS(input3["nums"]);
-console.log("Input: [" + input3.nums + "]");
-console.log("Result: " + result3);
-console.log("Expected: " + expected3);
-console.log("-".repeat(50));
+records.forEach((record, index) => {
+  console.log(`# Test case ${index + 1}`);
+  testSolution(record);
+  console.log("-".repeat(50));
+});

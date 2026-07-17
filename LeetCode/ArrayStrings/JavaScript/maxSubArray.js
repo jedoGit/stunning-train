041f3f4f68a712bdb753f336d635f3fcdeae0943
jedogit <jedogit@gmail.com>
2026-07-17 07:21:14 -0400

    Refactor max subarray JavaScript tests

--- LeetCode/ArrayStrings/JavaScript/maxSubArray.js
+++ LeetCode/ArrayStrings/JavaScript/maxSubArray.js
@@ -27,19 +27,54 @@
  * @param {number[]} nums
  * @return {number}
  */
-var maxSubArray = function (nums) {
-  let sum = 0;
-  let max = Number.MIN_SAFE_INTEGER;
+const Result = { PASS: "\x1b[92mPASS\x1b[0m", FAIL: "\x1b[91mFAIL\x1b[0m" };
 
-  for (let num of nums) {
-    sum += num;
-    // console.log(sum)
-    max = Math.max(sum, max);
-
-    if (sum < 0) {
-      sum = 0;
-    }
+class MaxSubArrayRecord {
+  constructor(nums, expected) {
+    this.nums = nums;
+    this.expected = expected;
   }
+}
 
-  return max;
-};
+class Solution {
+  maxSubArray(nums) {
+    let sum = 0;
+    let max = Number.MIN_SAFE_INTEGER;
+
+    for (let num of nums) {
+      sum += num;
+      max = Math.max(sum, max);
+
+      if (sum < 0) {
+        sum = 0;
+      }
+    }
+
+    return max;
+  }
+}
+
+function testSolution(record) {
+  const solution = new Solution();
+  const nums = [...record.nums];
+  const result = solution.maxSubArray(nums);
+  const pass = result === record.expected;
+
+  console.log(`Input: nums = ${JSON.stringify(record.nums)}`);
+  console.log(`Expected: ${record.expected}`);
+  console.log(`Result: ${result}`);
+  console.log(pass ? Result.PASS : Result.FAIL);
+}
+
+const records = [
+  new MaxSubArrayRecord([-2, 1, -3, 4, -1, 2, 1, -5, 4], 6),
+  new MaxSubArrayRecord([1], 1),
+  new MaxSubArrayRecord([5, 4, -1, 7, 8], 23),
+  new MaxSubArrayRecord([-2, -1], -1),
+];
+
+records.forEach((record, index) => {
+  console.log(`# Test case ${index + 1}`);
+  testSolution(record);
+  console.log("-".repeat(30));
+});

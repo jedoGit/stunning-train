af145ac3827574561a20a3b54521e738a0c799df
jedoGit <jedogit@gmail.com>
2026-07-29 06:11:41 -0400

    Refactor repeated substring pattern JavaScript tests

--- LeetCode/ArrayStrings/JavaScript/repeatedSubstringPattern.js
+++ LeetCode/ArrayStrings/JavaScript/repeatedSubstringPattern.js
@@ -20,38 +20,75 @@
 // 1 <= s.length <= 104
 // s consists of lowercase English letters.
 
-/**
- * @param {string} s
- * @return {boolean}
- */
 // TC: O(n) because at most, we have to repeat the substring (length 1) by n/1 times.
 // SC: O(n/2) at most, we have a substring of n/2 chars
 
-var repeatedSubstringPattern = function (s) {
-  // abab
+const Result = { PASS: "\x1b[92mPASS\x1b[0m", FAIL: "\x1b[91mFAIL\x1b[0m" };
 
-  // Let's start with the first char in s and append to our substring.
-  // Let's repeat it by length of s / lenght of substr.
-  // Then compare if it's equal to s.
+class RepeatedSubstringPatternRecord {
+  constructor(s, expected) {
+    this.s = s;
+    this.expected = expected;
+  }
+}
 
-  if (s.length < 2) {
+class Solution {
+  /**
+   * @param {string} s
+   * @return {boolean}
+   */
+  repeatedSubstringPattern(s) {
+    // abab
+
+    // Let's start with the first char in s and append to our substring.
+    // Let's repeat it by length of s / lenght of substr.
+    // Then compare if it's equal to s.
+
+    if (s.length < 2) {
+      return false;
+    }
+
+    let subStr = "";
+    const n = s.length;
+
+    // We only need to stop at the mid length of s.
+    for (let i = 0; i < n / 2; i += 1) {
+      subStr += s[i];
+
+      let rep = n / subStr.length;
+
+      if (subStr.repeat(rep) === s) {
+        // console.log(subStr, subStr.repeat(rep))
+        return true;
+      }
+    }
+
     return false;
   }
+}
 
-  let subStr = "";
-  const n = s.length;
+function testSolution(record) {
+  const solution = new Solution();
+  const result = solution.repeatedSubstringPattern(record.s);
+  const pass = result === record.expected;
 
-  // We only need to stop at the mid length of s.
-  for (let i = 0; i < n / 2; i += 1) {
-    subStr += s[i];
+  console.log(`Input: s = ${JSON.stringify(record.s)}`);
+  console.log(`Expected: ${record.expected}`);
+  console.log(`Result: ${result}`);
+  console.log(pass ? Result.PASS : Result.FAIL);
+}
 
-    let rep = n / subStr.length;
+const records = [
+  new RepeatedSubstringPatternRecord("abab", true),
+  new RepeatedSubstringPatternRecord("aba", false),
+  new RepeatedSubstringPatternRecord("abcabcabcabc", true),
+  new RepeatedSubstringPatternRecord("a", false),
+  new RepeatedSubstringPatternRecord("aa", true),
+  new RepeatedSubstringPatternRecord("abaababaab", true),
+];
 
-    if (subStr.repeat(rep) === s) {
-      // console.log(subStr, subStr.repeat(rep))
-      return true;
-    }
-  }
-
-  return false;
-};
+records.forEach((record, index) => {
+  console.log(`# Test case ${index + 1}`);
+  testSolution(record);
+  console.log("----------------------------------------");
+});

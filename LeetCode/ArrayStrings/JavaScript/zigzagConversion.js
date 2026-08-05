93aac2d79feb132616e9e14618c9a20057354703
jedoGit <jedogit@gmail.com>
2026-08-05 06:06:54 -0400

    Refactor zigzag conversion JavaScript tests

--- LeetCode/ArrayStrings/JavaScript/zigzagConversion.js
+++ LeetCode/ArrayStrings/JavaScript/zigzagConversion.js
@@ -36,31 +36,70 @@
 // TC: O(n) looping through the array only once
 // SC: O(1) in place processing
 
-/**
- * @param {string} s
- * @param {number} numRows
- * @return {string}
- */
-var convert = function (s, numRows) {
-  // Base case: If numRows is 1, we return s immediately
-  if (numRows === 1) return s;
+const Result = { PASS: "\x1b[92mPASS\x1b[0m", FAIL: "\x1b[91mFAIL\x1b[0m" };
 
-  let res = "";
+class ZigzagConversionRecord {
+  constructor(s, numRows, expected) {
+    this.s = s;
+    this.numRows = numRows;
+    this.expected = expected;
+  }
+}
 
-  // Setup the rows. We need it for calculation on the jump we need to
-  // do in the to assemble the result.
-  // The main calculation is increment = 2 * (numRows - 1) for the exterior elements of numRows[0] and numRows[end]
-  // For the middle elements of numRows, increment = increment - 2 * r
-  for (let r = 0; r < numRows; r++) {
-    let increment = 2 * (numRows - 1);
+class Solution {
+  /**
+   * @param {string} s
+   * @param {number} numRows
+   * @return {string}
+   */
+  convert(s, numRows) {
+    // Base case: If numRows is 1, we return s immediately
+    if (numRows === 1) return s;
 
-    for (let i = r; i < s.length; i += increment) {
-      res += s.at(i);
-      if (r > 0 && r < numRows - 1 && i + increment - 2 * r < s.length) {
-        res += s.at(i + increment - 2 * r);
+    let res = "";
+
+    // Setup the rows. We need it for calculation on the jump we need to
+    // do in the to assemble the result.
+    // The main calculation is increment = 2 * (numRows - 1) for the exterior elements of numRows[0] and numRows[end]
+    // For the middle elements of numRows, increment = increment - 2 * r
+    for (let r = 0; r < numRows; r++) {
+      let increment = 2 * (numRows - 1);
+
+      for (let i = r; i < s.length; i += increment) {
+        res += s.at(i);
+        if (r > 0 && r < numRows - 1 && i + increment - 2 * r < s.length) {
+          res += s.at(i + increment - 2 * r);
+        }
       }
     }
-  }
 
-  return res;
-};
+    return res;
+  }
+}
+
+function testSolution(record) {
+  const solution = new Solution();
+  const result = solution.convert(record.s, record.numRows);
+  const pass = result === record.expected;
+
+  console.log(
+    `Input: s = ${JSON.stringify(record.s)}, numRows = ${record.numRows}`
+  );
+  console.log(`Expected: ${JSON.stringify(record.expected)}`);
+  console.log(`Result: ${JSON.stringify(result)}`);
+  console.log(pass ? Result.PASS : Result.FAIL);
+}
+
+const records = [
+  new ZigzagConversionRecord("PAYPALISHIRING", 3, "PAHNAPLSIIGYIR"),
+  new ZigzagConversionRecord("PAYPALISHIRING", 4, "PINALSIGYAHRPI"),
+  new ZigzagConversionRecord("A", 1, "A"),
+  new ZigzagConversionRecord("AB", 1, "AB"),
+  new ZigzagConversionRecord("ABC", 2, "ACB"),
+];
+
+records.forEach((record, index) => {
+  console.log(`# Test case ${index + 1}`);
+  testSolution(record);
+  console.log("----------------------------------------");
+});

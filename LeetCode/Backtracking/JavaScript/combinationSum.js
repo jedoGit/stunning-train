8061c821df83fb697bccb67b023345aef9bbfdf1
jedoGit <jedogit@gmail.com>
2026-08-07 06:17:15 -0400

    Refactor combination sum JavaScript tests

--- LeetCode/Backtracking/JavaScript/combinationSum.js
+++ LeetCode/Backtracking/JavaScript/combinationSum.js
@@ -31,58 +31,100 @@
 // TC:
 // SC:
 
-/**
- * @param {number[]} candidates
- * @param {number} target
- * @return {number[][]}
- */
-var combinationSum = function (candidates, target) {
-  // For example, candidates = [2,3,6,7], target = 7
-  //                            2                         3                            6                             7
-  //                      2   3   6   7               3   6   7                      6   7                           7
-  //                2   3   6   7                 3   6   7
-  //          2   3   6   7
-  // so, [[2,2,3],[7]]
-  // 2 and 3 are candidates, and 2 + 2 + 3 = 7. Note that 2 can be used multiple times.
-  // 7 is a candidate, and 7 = 7.
-  // These are the only two combinations.
-  //
+const Result = { PASS: "\x1b[92mPASS\x1b[0m", FAIL: "\x1b[91mFAIL\x1b[0m" };
 
-  let res = [];
-
-  // helper function to dfs and backtrack through a path and check if sum of the path is equal to target.
-  // If equal, add to res array.
-  // If greater than, return, pop current comb and backtrack.
-  // If less than continue and push next index.
-  function backtrack(start, curSum, path) {
-    // test if curSum is equal to target
-    if (curSum === target) {
-      res.push([...path]);
-      return;
-    }
-
-    if (curSum > target) {
-      return;
-    }
-
-    // if curSum < target, continue below
-
-    for (let i = start; i < candidates.length; i++) {
-      curSum += candidates[i];
-      path.push(candidates[i]);
-
-      // We don't want to reuse a number, so after we're done with current level, we go to the next index i
-      backtrack(i, curSum, path);
-
-      curSum -= candidates[i];
-      path.pop();
-    }
-
-    return;
+class CombinationSumRecord {
+  constructor(candidates, target, expected) {
+    this.candidates = candidates;
+    this.target = target;
+    this.expected = expected;
   }
+}
 
-  // Call backtrack with empty comb array as input input
-  backtrack(0, 0, []);
+class Solution {
+  /**
+   * @param {number[]} candidates
+   * @param {number} target
+   * @return {number[][]}
+   */
+  combinationSum(candidates, target) {
+    // For example, candidates = [2,3,6,7], target = 7
+    //                            2                         3                            6                             7
+    //                      2   3   6   7               3   6   7                      6   7                           7
+    //                2   3   6   7                 3   6   7
+    //          2   3   6   7
+    // so, [[2,2,3],[7]]
+    // 2 and 3 are candidates, and 2 + 2 + 3 = 7. Note that 2 can be used multiple times.
+    // 7 is a candidate, and 7 = 7.
+    // These are the only two combinations.
+    //
 
-  return res;
-};
+    let res = [];
+
+    // helper function to dfs and backtrack through a path and check if sum of the path is equal to target.
+    // If equal, add to res array.
+    // If greater than, return, pop current comb and backtrack.
+    // If less than continue and push next index.
+    function backtrack(start, curSum, path) {
+      // test if curSum is equal to target
+      if (curSum === target) {
+        res.push([...path]);
+        return;
+      }
+
+      if (curSum > target) {
+        return;
+      }
+
+      // if curSum < target, continue below
+
+      for (let i = start; i < candidates.length; i++) {
+        curSum += candidates[i];
+        path.push(candidates[i]);
+
+        // We don't want to reuse a number, so after we're done with current level, we go to the next index i
+        backtrack(i, curSum, path);
+
+        curSum -= candidates[i];
+        path.pop();
+      }
+
+      return;
+    }
+
+    // Call backtrack with empty comb array as input input
+    backtrack(0, 0, []);
+
+    return res;
+  }
+}
+
+function testSolution(record) {
+  const solution = new Solution();
+  const result = solution.combinationSum([...record.candidates], record.target);
+  const pass = JSON.stringify(result) === JSON.stringify(record.expected);
+
+  console.log(
+    `Input: candidates = ${JSON.stringify(record.candidates)}, target = ${record.target}`
+  );
+  console.log(`Expected: ${JSON.stringify(record.expected)}`);
+  console.log(`Result: ${JSON.stringify(result)}`);
+  console.log(pass ? Result.PASS : Result.FAIL);
+}
+
+const records = [
+  new CombinationSumRecord([2, 3, 6, 7], 7, [[2, 2, 3], [7]]),
+  new CombinationSumRecord([2, 3, 5], 8, [
+    [2, 2, 2, 2],
+    [2, 3, 3],
+    [3, 5],
+  ]),
+  new CombinationSumRecord([2], 1, []),
+  new CombinationSumRecord([2, 3, 5], 5, [[2, 3], [5]]),
+];
+
+records.forEach((record, index) => {
+  console.log(`# Test case ${index + 1}`);
+  testSolution(record);
+  console.log("----------------------------------------");
+});

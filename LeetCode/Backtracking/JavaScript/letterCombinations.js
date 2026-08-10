039b4a3ba4e95fbdd3db70e28ecd0454e7a73e5f
jedoGit <jedogit@gmail.com>
2026-08-10 06:03:57 -0400

    Refactor letter combinations JavaScript tests

--- LeetCode/Backtracking/JavaScript/letterCombinations.js
+++ LeetCode/Backtracking/JavaScript/letterCombinations.js
@@ -23,47 +23,109 @@
 // TC: O(n*4^n) because at each level of the backtracking tree, you'll have a worst case 4^n leaf nodes the you'll do that for every digits, n is the length of digits
 // SC: O(n) you'll do recursion of n levels
 
-/**
- * @param {string} digits
- * @return {string[]}
- */
-var letterCombinations = function (digits) {
-  let res = [];
-  // This is a JS object to hold the mapping of digits to chars
-  const digitToChar = {
-    2: "abc",
-    3: "def",
-    4: "ghi",
-    5: "jkl",
-    6: "mno",
-    7: "pqrs",
-    8: "tuv",
-    9: "wxyz",
-  };
+const Result = { PASS: "\x1b[92mPASS\x1b[0m", FAIL: "\x1b[91mFAIL\x1b[0m" };
 
-  if (!digits) return res;
-
-  // We'll do a DFS on the backtracking tree and at each recursion, we'll pass a snapshot of the index i and the current string
-  function backtrack(i, curStr) {
-    // Base case: If you draw the backtracking tree, you'll see that the leaf node, you'll have digits.length number of
-    // chars in your curStr. I think i === digit.length will also work.
-    if (i === digits.length) {
-      res.push(curStr);
-      return;
-    }
-
-    // Access the char mapping of the current digit we're pointing to
-    const digitChar = digitToChar[digits[i]];
-
-    // We need to loop through all the chars mapped to this digit and backtrack (dfs) to the next level (i)
-    // Each time we recursively call backtrack, we append c
-    for (const c of digitChar) {
-      backtrack(i + 1, curStr + c);
-    }
+class LetterCombinationsRecord {
+  constructor(digits, expected) {
+    this.digits = digits;
+    this.expected = expected;
   }
+}
 
-  // Call backtrack with initally empty curStr
-  backtrack(0, "");
+class Solution {
+  /**
+   * @param {string} digits
+   * @return {string[]}
+   */
+  letterCombinations(digits) {
+    let res = [];
+    // This is a JS object to hold the mapping of digits to chars
+    const digitToChar = {
+      2: "abc",
+      3: "def",
+      4: "ghi",
+      5: "jkl",
+      6: "mno",
+      7: "pqrs",
+      8: "tuv",
+      9: "wxyz",
+    };
 
-  return res;
-};
+    if (!digits) return res;
+
+    // We'll do a DFS on the backtracking tree and at each recursion, we'll pass a snapshot of the index i and the current string
+    function backtrack(i, curStr) {
+      // Base case: If you draw the backtracking tree, you'll see that the leaf node, you'll have digits.length number of
+      // chars in your curStr. I think i === digit.length will also work.
+      if (i === digits.length) {
+        res.push(curStr);
+        return;
+      }
+
+      // Access the char mapping of the current digit we're pointing to
+      const digitChar = digitToChar[digits[i]];
+
+      // We need to loop through all the chars mapped to this digit and backtrack (dfs) to the next level (i)
+      // Each time we recursively call backtrack, we append c
+      for (const c of digitChar) {
+        backtrack(i + 1, curStr + c);
+      }
+    }
+
+    // Call backtrack with initally empty curStr
+    backtrack(0, "");
+
+    return res;
+  }
+}
+
+function testSolution(record) {
+  const solution = new Solution();
+  const result = solution.letterCombinations(record.digits);
+  const pass = JSON.stringify(result) === JSON.stringify(record.expected);
+
+  console.log(`Input: digits = "${record.digits}"`);
+  console.log(`Expected: ${JSON.stringify(record.expected)}`);
+  console.log(`Result: ${JSON.stringify(result)}`);
+  console.log(pass ? Result.PASS : Result.FAIL);
+}
+
+const records = [
+  new LetterCombinationsRecord("23", [
+    "ad",
+    "ae",
+    "af",
+    "bd",
+    "be",
+    "bf",
+    "cd",
+    "ce",
+    "cf",
+  ]),
+  new LetterCombinationsRecord("", []),
+  new LetterCombinationsRecord("2", ["a", "b", "c"]),
+  new LetterCombinationsRecord("79", [
+    "pw",
+    "px",
+    "py",
+    "pz",
+    "qw",
+    "qx",
+    "qy",
+    "qz",
+    "rw",
+    "rx",
+    "ry",
+    "rz",
+    "sw",
+    "sx",
+    "sy",
+    "sz",
+  ]),
+];
+
+records.forEach((record, index) => {
+  console.log(`# Test case ${index + 1}`);
+  testSolution(record);
+  console.log("----------------------------------------");
+});

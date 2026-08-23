8026668ff52e52baa4f36f2a10109ea96f2e96ff
jedoGit <jedogit@gmail.com>
2026-08-23 09:15:23 -0400

    Refactor build binary tree from preorder and inorder JavaScript tests

--- LeetCode/BinaryTreeDFS/JavaScript/binaryTreeFromPreorderAndInorder.js
+++ LeetCode/BinaryTreeDFS/JavaScript/binaryTreeFromPreorderAndInorder.js
@@ -19,36 +19,95 @@
 // preorder is guaranteed to be the preorder traversal of the tree.
 // inorder is guaranteed to be the inorder traversal of the tree.
 
+const Result = { PASS: "\x1b[92mPASS\x1b[0m", FAIL: "\x1b[91mFAIL\x1b[0m" };
+
 /**
  * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
  */
-/**
- * @param {number[]} preorder
- * @param {number[]} inorder
- * @return {TreeNode}
- */
-var buildTree = function (preorder, inorder) {
-  if (preorder.length === 0 && inorder.length === 0) return null;
+class TreeNode {
+  constructor(val, left, right) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
 
-  // in the preorder array, index 0 is always the root
-  let root = new TreeNode(preorder[0]);
-  // console.log("root: ", root)
-  // console.log("root: " + root + " root.value: " + root.value)
-  // what we want to do here is find the index of the root value in the preorder array. This will point to the root and we can use this to partition the preorder array
-  let mid = inorder.indexOf(preorder[0]);
-  // console.log("mid: " + mid + " inorder[mid]: " + inorder[mid])
-  // for the left child, we call buildTree recursively
-  // console.log("left: " + preorder.slice(1, mid+1) + " " + inorder.slice(0,mid))
-  root.left = buildTree(preorder.slice(1, mid + 1), inorder.slice(0, mid));
-  // console.log("left: ", root.left)
-  // for the right child, we call buildTree recursively
-  root.right = buildTree(preorder.slice(mid + 1), inorder.slice(mid + 1));
-  // console.log("right: ", root.right)
+class BuildTreeRecord {
+  constructor(preorder, inorder, expected) {
+    this.preorder = preorder;
+    this.inorder = inorder;
+    this.expected = expected;
+  }
+}
 
-  return root;
-};
+class Solution {
+  /**
+   * @param {number[]} preorder
+   * @param {number[]} inorder
+   * @return {TreeNode}
+   */
+  buildTree(preorder, inorder) {
+    if (preorder.length === 0 && inorder.length === 0) return null;
+
+    // in the preorder array, index 0 is always the root
+    let root = new TreeNode(preorder[0]);
+    // what we want to do here is find the index of the root value in the inorder array. This will point to the root and we can use this to partition the preorder array
+    let mid = inorder.indexOf(preorder[0]);
+    // for the left child, we call buildTree recursively
+    root.left = this.buildTree(preorder.slice(1, mid + 1), inorder.slice(0, mid));
+    // for the right child, we call buildTree recursively
+    root.right = this.buildTree(preorder.slice(mid + 1), inorder.slice(mid + 1));
+
+    return root;
+  }
+}
+
+function treeToArray(root) {
+  if (!root) return [];
+
+  const values = [];
+  const queue = [root];
+
+  while (queue.length) {
+    const node = queue.shift();
+
+    if (node) {
+      values.push(node.val);
+      queue.push(node.left);
+      queue.push(node.right);
+    } else {
+      values.push(null);
+    }
+  }
+
+  // Drop the trailing nulls so the shape matches the LeetCode serialization
+  while (values.length && values[values.length - 1] === null) {
+    values.pop();
+  }
+
+  return values;
+}
+
+function testSolution(record) {
+  const solution = new Solution();
+  const result = treeToArray(solution.buildTree([...record.preorder], [...record.inorder]));
+  const pass = JSON.stringify(result) === JSON.stringify(record.expected);
+
+  console.log(`Input: preorder = ${JSON.stringify(record.preorder)}, inorder = ${JSON.stringify(record.inorder)}`);
+  console.log(`Expected: ${JSON.stringify(record.expected)}`);
+  console.log(`Result: ${JSON.stringify(result)}`);
+  console.log(pass ? Result.PASS : Result.FAIL);
+}
+
+const records = [
+  new BuildTreeRecord([3, 9, 20, 15, 7], [9, 3, 15, 20, 7], [3, 9, 20, null, null, 15, 7]),
+  new BuildTreeRecord([-1], [-1], [-1]),
+  new BuildTreeRecord([1, 2], [2, 1], [1, 2]),
+  new BuildTreeRecord([1, 2, 3], [2, 1, 3], [1, 2, 3]),
+];
+
+records.forEach((record, index) => {
+  console.log(`# Test case ${index + 1}`);
+  testSolution(record);
+  console.log("----------------------------------------");
+});

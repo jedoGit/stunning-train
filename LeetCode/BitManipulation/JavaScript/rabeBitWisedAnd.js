bc7dfd7557c837f0bb1fef1d7d0bca1de6121e2a
jedoGit <jedogit@gmail.com>
2026-09-10 07:15:45 -0400

    Refactor range bitwise AND JavaScript tests

--- LeetCode/BitManipulation/JavaScript/rabeBitWisedAnd.js
+++ LeetCode/BitManipulation/JavaScript/rabeBitWisedAnd.js
@@ -20,17 +20,58 @@
 // TC: O(1)
 // SC: O(1)
 
-/**
- * @param {number} left
- * @param {number} right
- * @return {number}
- */
-var rangeBitwiseAnd = function (left, right) {
-  let cnt = 0;
-  while (left !== right) {
-    left >>= 1;
-    right >>= 1;
-    cnt++;
+const Result = { PASS: "\x1b[92mPASS\x1b[0m", FAIL: "\x1b[91mFAIL\x1b[0m" };
+
+class RangeBitwiseAndRecord {
+  constructor(left, right, expected) {
+    this.left = left;
+    this.right = right;
+    this.expected = expected;
   }
-  return left << cnt;
-};
+}
+
+class Solution {
+  /**
+   * @param {number} left
+   * @param {number} right
+   * @return {number}
+   */
+  rangeBitwiseAnd(left, right) {
+    let tempLeft = left;
+    let tempRight = right;
+    let cnt = 0;
+
+    while (tempLeft !== tempRight) {
+      tempLeft >>= 1;
+      tempRight >>= 1;
+      cnt++;
+    }
+
+    return tempLeft << cnt;
+  }
+}
+
+function testSolution(record) {
+  const solution = new Solution();
+  const result = solution.rangeBitwiseAnd(record.left, record.right);
+  const pass = result === record.expected;
+
+  console.log(`Input: left = ${record.left}, right = ${record.right}`);
+  console.log(`Expected: ${record.expected}`);
+  console.log(`Result: ${result}`);
+  console.log(pass ? Result.PASS : Result.FAIL);
+}
+
+const records = [
+  new RangeBitwiseAndRecord(5, 7, 4),
+  new RangeBitwiseAndRecord(0, 0, 0),
+  new RangeBitwiseAndRecord(1, 2147483647, 0),
+  new RangeBitwiseAndRecord(20, 23, 20),
+  new RangeBitwiseAndRecord(10, 10, 10),
+];
+
+records.forEach((record, index) => {
+  console.log(`# Test case ${index + 1}`);
+  testSolution(record);
+  console.log("----------------------------------------");
+});

5a180dc136bcffb4931d0c228a109147da10f4f0
jedoGit <jedogit@gmail.com>
2026-09-16 07:16:01 -0400

    Refactor kth smallest element in BST JavaScript tests

--- LeetCode/BST/JavaScript/kthSmallestElementBST.js
+++ LeetCode/BST/JavaScript/kthSmallestElementBST.js
@@ -20,36 +20,98 @@
 // TC: O(n)
 // SC: O(h), h is height of the tree
 
-/**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
- */
-/**
- * @param {TreeNode} root
- * @param {number} k
- * @return {number}
- */
-var kthSmallest = function (root, k) {
-  let n = 0;
-  let s = [];
-  let cur = root;
+const Result = { PASS: "\x1b[92mPASS\x1b[0m", FAIL: "\x1b[91mFAIL\x1b[0m" };
 
-  while (cur !== null || s.length > 0) {
-    while (cur !== null) {
-      s.push(cur);
-      cur = cur.left;
-    }
-
-    cur = s.pop();
-    n += 1;
-
-    if (n === k) {
-      return cur.val;
-    }
-    cur = cur.right;
+class KthSmallestRecord {
+  constructor(root, k, expected) {
+    this.root = root;
+    this.k = k;
+    this.expected = expected;
   }
-};
+}
+
+class TreeNode {
+  constructor(val, left = null, right = null) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+class Solution {
+  /**
+   * @param {TreeNode} root
+   * @param {number} k
+   * @return {number}
+   */
+  kthSmallest(root, k) {
+    let n = 0;
+    let s = [];
+    let cur = root;
+
+    while (cur !== null || s.length > 0) {
+      while (cur !== null) {
+        s.push(cur);
+        cur = cur.left;
+      }
+
+      cur = s.pop();
+      n += 1;
+
+      if (n === k) {
+        return cur.val;
+      }
+      cur = cur.right;
+    }
+  }
+}
+
+function buildTree(values) {
+  if (values.length === 0 || values[0] === null) {
+    return null;
+  }
+
+  const root = new TreeNode(values[0]);
+  const queue = [root];
+  let index = 1;
+
+  while (queue.length > 0 && index < values.length) {
+    const node = queue.shift();
+
+    if (values[index] !== null) {
+      node.left = new TreeNode(values[index]);
+      queue.push(node.left);
+    }
+    index++;
+
+    if (index < values.length && values[index] !== null) {
+      node.right = new TreeNode(values[index]);
+      queue.push(node.right);
+    }
+    index++;
+  }
+
+  return root;
+}
+
+function testSolution(record) {
+  console.log(`input:\troot: ${JSON.stringify(record.root)}, k: ${record.k}`);
+  console.log(`expected: ${record.expected}`);
+
+  const solution = new Solution();
+  const result = solution.kthSmallest(buildTree(record.root), record.k);
+
+  console.log(`result: ${result}`);
+  console.log(result === record.expected ? Result.PASS : Result.FAIL);
+}
+
+const records = [
+  new KthSmallestRecord([3, 1, 4, null, 2], 1, 1),
+  new KthSmallestRecord([5, 3, 6, 2, 4, null, null, 1], 3, 3),
+];
+
+records.forEach((record, index) => {
+  console.log(`# Test case ${index + 1}`);
+  testSolution(record);
+  console.log("----------------------------------------");
+});

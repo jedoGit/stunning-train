2bedf28dd2fd3ab613d4ae724de855c7c57be36e
jedoGit <jedogit@gmail.com>
2026-09-18 07:15:47 -0400

    Refactor house robber JavaScript tests

--- LeetCode/DP1D/JavaScript/houseRobber.js
+++ LeetCode/DP1D/JavaScript/houseRobber.js
@@ -23,34 +23,70 @@
 // TC: O(n), loop through n
 // SC: O(1), in place processing
 
-/**
- * @param {number[]} nums
- * @return {number}
- */
-var rob = function (nums) {
-  // The DP solution of this problem is not very intuitive. You'll need to study it and understand it fully.
-  // [rob1, rob2], at each iteration, you'll update these two variables.
-  // For example, nums = [2,7,9,3,1], we can't rob houses that are next to each other.
-  // So, to maximize our larceny, we do it like this:
-  // This is the first sub-problem of the DP:
-  // [2, skip, max(9,3,1)] and/or [skip, 7, skip, max(3,1)]
-  // We can break down the sub-problem further:
-  // [2, skip, 9] and/or [skip, 7, skip] compute [rob1, rob2]
-  // We're going to compute [rob1, rob2, n, n+1, n+2, ...], initialize rob1 and rob2 to 0
-  // Visually, it will look like this: [rob1, rob2, 2, 7, 9, 3, 1]
-  // The max of rob1 + 2 and rob2, assign that to rob2
-  // The previous value of rob2, assign that to rob1
-  // Do this for each value n
+const Result = { PASS: "\x1b[92mPASS\x1b[0m", FAIL: "\x1b[91mFAIL\x1b[0m" };
 
-  // Initilize these two values with zero
-  let [rob1, rob2] = [0, 0];
-
-  for (const num of nums) {
-    const temp = Math.max(rob1 + num, rob2);
-    rob1 = rob2;
-    rob2 = temp;
+class HouseRobberRecord {
+  constructor(nums, expected) {
+    this.nums = nums;
+    this.expected = expected;
   }
+}
 
-  // The max value will always be in rob2
-  return rob2;
-};
+class Solution {
+  /**
+   * @param {number[]} nums
+   * @return {number}
+   */
+  rob(nums) {
+    // The DP solution of this problem is not very intuitive. You'll need to study it and understand it fully.
+    // [rob1, rob2], at each iteration, you'll update these two variables.
+    // For example, nums = [2,7,9,3,1], we can't rob houses that are next to each other.
+    // So, to maximize our larceny, we do it like this:
+    // This is the first sub-problem of the DP:
+    // [2, skip, max(9,3,1)] and/or [skip, 7, skip, max(3,1)]
+    // We can break down the sub-problem further:
+    // [2, skip, 9] and/or [skip, 7, skip] compute [rob1, rob2]
+    // We're going to compute [rob1, rob2, n, n+1, n+2, ...], initialize rob1 and rob2 to 0
+    // Visually, it will look like this: [rob1, rob2, 2, 7, 9, 3, 1]
+    // The max of rob1 + 2 and rob2, assign that to rob2
+    // The previous value of rob2, assign that to rob1
+    // Do this for each value n
+
+    // Initilize these two values with zero
+    let [rob1, rob2] = [0, 0];
+
+    for (const num of nums) {
+      const temp = Math.max(rob1 + num, rob2);
+      rob1 = rob2;
+      rob2 = temp;
+    }
+
+    // The max value will always be in rob2
+    return rob2;
+  }
+}
+
+function testSolution(record) {
+  const solution = new Solution();
+  const result = solution.rob(record.nums);
+  const pass = result === record.expected;
+
+  console.log(`Input: nums = ${JSON.stringify(record.nums)}`);
+  console.log(`Expected: ${record.expected}`);
+  console.log(`Result: ${result}`);
+  console.log(pass ? Result.PASS : Result.FAIL);
+}
+
+const records = [
+  new HouseRobberRecord([1, 2, 3, 1], 4),
+  new HouseRobberRecord([2, 7, 9, 3, 1], 12),
+  new HouseRobberRecord([5], 5),
+  new HouseRobberRecord([2, 1, 1, 2], 4),
+  new HouseRobberRecord([0, 0, 0], 0),
+];
+
+records.forEach((record, index) => {
+  console.log(`# Test case ${index + 1}`);
+  testSolution(record);
+  console.log("----------------------------------------");
+});
